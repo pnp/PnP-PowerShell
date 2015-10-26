e42731060e62e91b4e0625babee6ca82edd0622d
Daniel Laskewitz <daniel@laskewitz.nl>
2015-10-26 18:21:29 +0100

    Updated documentation for Remove-SPONavigationNode, Set-SPOMinimalDownloadStrategy & Remove-SPOContentType

--- Commands/Branding/RemoveNavigationNode.cs
+++ Commands/Branding/RemoveNavigationNode.cs
@@ -10,6 +10,12 @@ namespace OfficeDevPnP.PowerShell.Commands
     [Cmdlet(VerbsCommon.Remove, "SPONavigationNode", SupportsShouldProcess = true)]
     [CmdletHelp("Removes a menu item from either the quicklaunch or top navigation", 
         Category = CmdletHelpCategory.Branding)]
+    [CmdletExample(Code = @"PS:> Remove-SPONavigationNode -Title Recent -Location QuickLaunch",
+        Remarks = "Will remove the recent navigation node from the quick launch in the current web.",
+        SortOrder = 1)]
+    [CmdletExample(Code = @"PS:> Remove-SPONavigationNode -Title Home -Location TopNavigationBar -Force",
+        Remarks = "Will remove the home navigation node from the top navigation bar without prompting for a confirmation in the current web.",
+        SortOrder = 2)]
     public class RemoveNavigationNode : SPOWebCmdlet
     {
         [Parameter(Mandatory = true)]
