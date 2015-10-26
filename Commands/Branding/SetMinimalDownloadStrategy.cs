315d519d4e983ca1c7bc5fbe271d14d1428a72db
Daniel Laskewitz <daniel@laskewitz.nl>
2015-10-26 18:17:32 +0100

    Revert "Updated documentation for RemoveNavigationNode, SetMinimalDownloadStrategy & RemoveContentType"
    This reverts commit dd8cf1ec00e195969be7702b989e2a8325a66b4a.

--- Commands/Branding/SetMinimalDownloadStrategy.cs
+++ Commands/Branding/SetMinimalDownloadStrategy.cs
@@ -10,7 +10,7 @@ namespace OfficeDevPnP.PowerShell.Commands
     [CmdletHelp("Activates or deactivates the minimal downloading strategy.", 
         Category = CmdletHelpCategory.Branding)]
     [CmdletExample(Code = @"PS:> Set-SPOMinimalDownloadStrategy -Off",
-        Remarks = "Will deactivate minimal download strategy (MDS) for the current web.",
+        Remarks = "Will deactivate minimal download strategy (MDS) for the current site.",
         SortOrder = 1)]
     public class SetMDS : SPOWebCmdlet
     {
