1aef48a6ae7767f056df7d486b9ed85bd61b0d02
Koen Zomers <mail@koenzomers.nl>
2015-12-15 19:02:06 +0100

    Added help text for the parameters

--- Commands/Branding/AddJavaScriptLink.cs
+++ Commands/Branding/AddJavaScriptLink.cs
@@ -11,14 +11,14 @@ namespace OfficeDevPnP.PowerShell.Commands
         Category = CmdletHelpCategory.Branding)]
     public class AddJavaScriptLink : SPOWebCmdlet
     {
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, HelpMessage = "Name under which to register the JavaScriptLink")]
         [Alias("Key")]
         public string Name = string.Empty;
 
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, HelpMessage = "URL to the JavaScript file to inject")]
         public string[] Url = null;
 
-        [Parameter(Mandatory = false)]
+        [Parameter(Mandatory = false, HelpMessage = "Sequence of this JavaScript being injected. Use when you have a specific sequence with which to have JavaScript files being added to the page. I.e. jQuery library first and then jQueryUI.")]
         public int Sequence = 0;
 
         [Parameter(Mandatory = false)]
@@ -26,7 +26,7 @@ namespace OfficeDevPnP.PowerShell.Commands
         [Alias("AddToSite")]
         public SwitchParameter SiteScoped;
 
-        [Parameter(Mandatory = false)]
+        [Parameter(Mandatory = false, HelpMessage = "Defines if this JavaScript file will be injected to every page within the current site collection or web. Default is web.")]
         public CustomActionScope Scope = CustomActionScope.Web;
 
         protected override void ExecuteCmdlet()
