8885a617e69b2258fb71bfa3648c4d883d69f7c7
Koen Zomers <mail@koenzomers.nl>
2015-12-16 01:12:21 +0100

    Added support for using an oAuth token in the unit tests

--- Tests/PSTestScope.cs
+++ Tests/PSTestScope.cs
@@ -14,11 +14,17 @@ namespace OfficeDevPnP.PowerShell.Tests
 
         public string SiteUrl { get; set; }
         public string CredentialManagerEntry { get; set; }
+        public string Realm { get; set; }
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
 
         public PSTestScope(bool connect = true)
         {
             SiteUrl = ConfigurationManager.AppSettings["SPODevSiteUrl"];
             CredentialManagerEntry = ConfigurationManager.AppSettings["SPOCredentialManagerLabel"];
+            Realm = ConfigurationManager.AppSettings["Realm"];
+            AppId = ConfigurationManager.AppSettings["AppId"];
+            AppSecret = ConfigurationManager.AppSettings["AppSecret"];
 
             var iss = InitialSessionState.CreateDefault();
             if (connect)
@@ -39,7 +45,18 @@ namespace OfficeDevPnP.PowerShell.Tests
                 cmd.Parameters.Add("Url", SiteUrl);
                 if (!string.IsNullOrEmpty(CredentialManagerEntry))
                 {
+                    // Use Windows Credential Manager to authenticate
                     cmd.Parameters.Add("Credentials", CredentialManagerEntry);
+                }
+                else
+                {                    
+                    if (!string.IsNullOrEmpty(Realm) && !string.IsNullOrEmpty("AppId") && !string.IsNullOrEmpty("AppSecret"))
+                    {
+                        // Use oAuth Token to authenticate
+                        cmd.Parameters.Add("Realm", Realm);
+                        cmd.Parameters.Add("AppId", AppId);
+                        cmd.Parameters.Add("AppSecret", AppSecret);
+                    }
                 }
                 pipeLine.Commands.Add(cmd);
                 pipeLine.Invoke();
