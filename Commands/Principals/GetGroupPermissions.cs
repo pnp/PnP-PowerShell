fed8c077bad8b2c056cc9365af81fc539794cf40
Koen Zomers <mail@koenzomers.nl>
2015-12-17 22:21:44 +0100

    Added Get-SPOGroupPermissions command to retrieve the permissions assigned to a SharePoint Group. This functionality was not yet exposed.

---
+++ Commands/Principals/GetGroupPermissions.cs
@@ -0,0 +1,32 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace OfficeDevPnP.PowerShell.Commands.Principals
+{
+    [Cmdlet(VerbsCommon.Get, "SPOGroupPermissions")]
+    [CmdletHelp("Returns the permissions for a specific group",
+        Category = CmdletHelpCategory.Principals)]
+    [CmdletExample(
+        Code = @"PS:> Get-SPOGroupPermissions -Identity 'My Site Members'", 
+        Remarks = "Returns the permissions for the group with the name 'My Site Members'",
+        SortOrder = 0)]
+    public class GetGroupPermissions : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "ByName", HelpMessage = "Get the permissions of a specific group by name")]
+        [Alias("Name")]
+        public GroupPipeBind Identity = new GroupPipeBind();
+
+        protected override void ExecuteCmdlet()
+        {
+            var group = Identity.GetGroup(SelectedWeb);
+            var roleAssignment = SelectedWeb.RoleAssignments.GetByPrincipal(group);
+            var roleDefinitionBindings = roleAssignment.RoleDefinitionBindings;
+            ClientContext.Load(roleDefinitionBindings);
+            ClientContext.ExecuteQueryRetry();
+
+            WriteObject(roleDefinitionBindings, true);
+        }
+    }
+}
