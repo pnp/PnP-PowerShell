35d476c40ee8a77270b2823982d732a535bb0d16
wobba <miksvenson@gmail.com>
2016-08-05 09:57:42 +0200

    Fixed merge conflict, and added cmdlet example for setting site collection owner

--- Commands/Admin/SetTenantSite.cs
+++ Commands/Admin/SetTenantSite.cs
@@ -19,6 +19,9 @@ namespace SharePointPnP.PowerShell.Commands
     [CmdletExample(
       Code = @"PS:> Set-SPOTenantSite -Url https://contoso.sharepoint.com -Title 'Contoso Website' -StorageWarningLevel 8000 -StorageMaximumLevel 10000",
       Remarks = @"This will set the title of the site collection with the URL 'https://contoso.sharepoint.com' to 'Contoso Website', set the storage warning level to 8GB and set the storage maximum level to 10GB.", SortOrder = 2)]
+    [CmdletExample(
+      Code = @"PS:> Set-SPOTenantSite -Url https://contoso.sharepoint.com/sites/sales -Owners 'i:0#.f|membership|user@contoso.onmicrosoft.com'",
+      Remarks = @"This will set user@contoso.onmicrosoft.com as a site collection owner at 'https://contoso.sharepoint.com/sites/sales'.", SortOrder = 3)]   
     public class SetTenantSite : SPOAdminCmdlet
     {
         [Parameter(Mandatory = false, HelpMessage = "Specifies the URL of the site", Position = 0, ValueFromPipeline = true)]
