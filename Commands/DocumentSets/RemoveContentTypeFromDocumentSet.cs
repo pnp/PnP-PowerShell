995d6b4e6252ff3e81e5099e1bd0a2e4afb33233
Erwin van Hunen <erwin.van.hunen@outlook.com>
2016-09-04 22:31:40 +0200

    Removed non-applicable example

--- Commands/DocumentSets/RemoveContentTypeFromDocumentSet.cs
+++ Commands/DocumentSets/RemoveContentTypeFromDocumentSet.cs
@@ -13,15 +13,9 @@ namespace SharePointPnP.PowerShell.Commands.DocumentSets
         Remarks = "This will remove the content type called 'Test CT' from the document set called ''Test Document Set'",
         SortOrder = 1)]
     [CmdletExample(
-        Code = @"PS:> $docset = Remove-SPODocumentSetTemplate -Identity ""Test Document Set""
-PS:> $ct = Get-SPOContentType -Identity ""Test CT""
-PS:> Add-SPOContentTypeToDocumentSet -ContentType $ct -DocumentSet $docset",
-        Remarks = "This will remove the content type called 'Test CT' from the document set called ''Test Document Set'",
-        SortOrder = 2)]
-    [CmdletExample(
         Code = @"PS:> Remove-SPOContentTypeFromDocumentSet -ContentType 0x0101001F1CEFF1D4126E4CAD10F00B6137E969 -DocumentSet 0x0120D520005DB65D094035A241BAC9AF083F825F3B",
         Remarks = "This will remove the content type called 'Test CT' from the document set called ''Test Document Set'",
-        SortOrder = 3)]
+        SortOrder = 2)]
     public class RemoveContentTypeFromDocumentSet : SPOWebCmdlet
     {
         [Parameter(Mandatory = true, HelpMessage = "The content type to remove. Either specify name, an id, or a content type object.")]
