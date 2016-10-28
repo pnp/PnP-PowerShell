d2d995ef15ecc0e1eecd1d3ab071110f85948cee
Erwin van Hunen <erwin.van.hunen@outlook.com>
2016-10-28 13:47:43 +0200

    Added cmdlet alias attribute

---
+++ HelpAttributes/CmdletAliasAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.PowerShell.CmdletHelpAttributes
+{
+    public class CmdletAliasAttribute
+    {
+        private string _alias;
+        public CmdletAliasAttribute(string alias)
+        {
+            _alias = alias;
+        }
+    }
+}
