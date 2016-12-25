021ab46b4d9b499778dfa282697e50f21fe46b2e
skaggej <eskaggs@outlook.com>
2016-12-24 22:47:14 -0700

    Updated documentation remarks, public variable names, and call to Core method.

--- Commands/Publishing/AddPublishingImageRendition.cs
+++ Commands/Publishing/AddPublishingImageRendition.cs
@@ -5,25 +5,24 @@ using SharePointPnP.PowerShell.CmdletHelpAttributes;
 namespace SharePointPnP.PowerShell.Commands.Publishing
 {
     [Cmdlet(VerbsCommon.Add, "PnPPublishingImageRendition")]
-    [CmdletHelp("Adds an Image Rendition", Category = CmdletHelpCategory.Publishing)]
+    [CmdletHelp("Adds an Image Rendition if the Name of the Image Rendition does not already exist. This prevents creating two Image Renditions that share the same name.", Category = CmdletHelpCategory.Publishing)]
     [CmdletExample(
-        Code = @"PS:> Add-PnPPublishingImageRendition -ImageRenditionName ""MyImageRendition"" -Width 800 -Height 600",
-        Remarks = @"Adds an Image Rendition.",
+        Code = @"PS:> Add-PnPPublishingImageRendition -Name ""MyImageRendition"" -Width 800 -Height 600",
         SortOrder = 1)]
     public class AddPublishingImageRendition : SPOWebCmdlet
     {
         [Parameter(Mandatory = true, HelpMessage = "The display name of the Image Rendition.")]
-        public string imageRenditionName = string.Empty;
+        public string Name = string.Empty;
 
         [Parameter(Mandatory = true, HelpMessage = "The width of the Image Rendition.")]
-        public int imageRenditionWidth = 0;
+        public int Width = 0;
         
         [Parameter(Mandatory = true, HelpMessage = "The height of the Image Rendition.")]
-        public int imageRenditionHeight = 0;
+        public int Height = 0;
 
         protected override void ExecuteCmdlet()
         {
-            SelectedWeb.AddPublishingImageRendition(imageRenditionName, imageRenditionWidth, imageRenditionHeight);
+            SelectedWeb.CreatePublishingImageRendition(Name, Width, Height);
         }
     }
 }
--- Documentation/AddPnPPublishingImageRendition.md
+++ Documentation/AddPnPPublishingImageRendition.md
@@ -1,10 +1,10 @@
 #Add-PnPPublishingImageRendition
-Adds an Image Rendition
+Adds an Image Rendition if the Name of the Image Rendition does not already exist. This prevents creating two Image Renditions that share the same name.
 ##Syntax
 ```powershell
-Add-PnPPublishingImageRendition -imageRenditionName <String>
-                                -imageRenditionWidth <Int32>
-                                -imageRenditionHeight <Int32>
+Add-PnPPublishingImageRendition -Name <String>
+                                -Width <Int32>
+                                -Height <Int32>
                                 [-Web <WebPipeBind>]
 ```
 
@@ -12,14 +12,14 @@ Add-PnPPublishingImageRendition -imageRenditionName <String>
 ##Parameters
 Parameter|Type|Required|Description
 ---------|----|--------|-----------
-|imageRenditionHeight|Int32|True|The height of the Image Rendition.|
-|imageRenditionName|String|True|The display name of the Image Rendition.|
-|imageRenditionWidth|Int32|True|The width of the Image Rendition.|
+|Height|Int32|True|The height of the Image Rendition.|
+|Name|String|True|The display name of the Image Rendition.|
 |Web|WebPipeBind|False|The web to apply the command to. Omit this parameter to use the current web.|
+|Width|Int32|True|The width of the Image Rendition.|
 ##Examples
 
 ###Example 1
 ```powershell
-PS:> Add-PnPPublishingImageRendition -ImageRenditionName "MyImageRendition" -Width 800 -Height 600
+PS:> Add-PnPPublishingImageRendition -Name "MyImageRendition" -Width 800 -Height 600
 ```
-Adds an Image Rendition.
+
