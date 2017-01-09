ae330b16c16b6372323059be04696b372cb31079
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-01-09 14:17:51 +0100

    Clear drivestate when issueuing disconnect-pnponline

--- Commands/Base/DisconnectSPOnline.cs
+++ Commands/Base/DisconnectSPOnline.cs
@@ -1,6 +1,9 @@
 ﻿using SharePointPnP.PowerShell.CmdletHelpAttributes;
 using System;
+using System.Linq;
 using System.Management.Automation;
+using System.Reflection;
+using SharePointPnP.PowerShell.Commands.Provider;
 
 namespace SharePointPnP.PowerShell.Commands.Base
 {
@@ -18,6 +21,18 @@ namespace SharePointPnP.PowerShell.Commands.Base
         {
             if (!DisconnectCurrentService())
                 throw new InvalidOperationException(Properties.Resources.NoConnectionToDisconnect);
+
+            var provider = SessionState.Provider.GetAll().FirstOrDefault(p => p.Name.Equals("SPO", StringComparison.InvariantCultureIgnoreCase));
+            if (provider != null)
+            {
+                var drives = provider.Drives.Where(d => d.Provider.Module.ImplementingAssembly.FullName == Assembly.GetExecutingAssembly().FullName);
+                foreach (var drive in drives)
+                {
+                    var spoDrive = drive as SPODriveInfo;
+                    spoDrive.ClearState();
+                    SessionState.Drive.Remove(drive.Name, true, "Global");
+                }
+            }
         }
 
         internal static bool DisconnectCurrentService()
