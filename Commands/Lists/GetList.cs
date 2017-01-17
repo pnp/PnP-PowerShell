a19703668288673f768821c702704f6b013dc822
Michael Rees Pullen <mrpullen@outlook.com>
2017-01-17 11:56:40 -0500

    Updated Get-PnPList Cmdlet to have an -Includes parameter which builds a dynamic linq query to include user specified properties.

--- Commands/Lists/GetList.cs
+++ Commands/Lists/GetList.cs
@@ -28,17 +28,83 @@ namespace SharePointPnP.PowerShell.Commands.Lists
         [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 0, HelpMessage = "The ID, name or Url (Lists/MyList) of the list.")]
         public ListPipeBind Identity;
 
+        [Parameter(Mandatory = false, ValueFromPipeline = false, Position = 1, HelpMessage = "Additional Properties to retrieve for the List item(s) to be returned")]
+        [ValidateSet("ContentTypes", "DataSource", "EventReceivers", "Fields", "SchemaXml")]
+        public string[] Includes;
+
         protected override void ExecuteCmdlet()
         {
             if (Identity != null)
             {
                 var list = Identity.GetList(SelectedWeb);
+
+                if (Includes.Length > 0)
+                {
+                    foreach (var include in Includes)
+                    {
+                        switch (include)
+                        {
+                            case "ContentTypes":
+                                ClientContext.Load(list, l => l.ContentTypes);
+                                break;
+                            case "DataSource":
+                                ClientContext.Load(list, l => l.DataSource);
+                                break;
+                            case "EventReceivers":
+                                ClientContext.Load(list, l => l.EventReceivers);
+                                break;
+                            case "Fields":
+                                ClientContext.Load(list, l => l.Fields);
+                                break;
+                            case "SchemaXml":
+                                ClientContext.Load(list, l => l.SchemaXml);
+                                break;
+                            default: break;
+                        }
+                    }
+                    ClientContext.ExecuteQueryRetry();
+                }
+
+
                 WriteObject(list);
 
             }
             else
             {
-                var lists = ClientContext.LoadQuery(SelectedWeb.Lists.IncludeWithDefaultProperties(l => l.Id, l => l.BaseTemplate, l => l.OnQuickLaunch, l => l.DefaultViewUrl, l => l.Title, l => l.Hidden, l => l.RootFolder.ServerRelativeUrl));
+
+                var query = (SelectedWeb.Lists.IncludeWithDefaultProperties(l => l.Id, l => l.BaseTemplate, l => l.OnQuickLaunch, l => l.DefaultViewUrl, l => l.Title, l => l.Hidden, l => l.RootFolder.ServerRelativeUrl));
+
+                //var lists = ClientContext.LoadQuery((SelectedWeb.Lists.IncludeWithDefaultProperties(l => l.Id, l => l.BaseTemplate, l => l.OnQuickLaunch, l => l.DefaultViewUrl, l => l.Title, l => l.Hidden, l => l.RootFolder.ServerRelativeUrl));
+
+                if (Includes.Length > 0)
+                {
+                    foreach (var include in Includes)
+                    {
+                        switch (include)
+                        {
+                            case "ContentTypes":
+                                query.Include(l => l.ContentTypes);
+                                break;
+                            case "DataSource":
+                                query.Include(l => l.DataSource);
+                                break;
+                            case "EventReceivers":
+                                query.Include(l => l.EventReceivers);
+
+                                break;
+                            case "Fields":
+                                query.Include(l => l.Fields);
+                                break;
+                            case "SchemaXml":
+                                query.Include(l => l.SchemaXml);
+
+                                break;
+                            default: break;
+                        }
+                    }
+                    ClientContext.ExecuteQueryRetry();
+                }
+                var lists = ClientContext.LoadQuery(query);
                 ClientContext.ExecuteQueryRetry();
                 WriteObject(lists, true);
             }
