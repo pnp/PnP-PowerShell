d53bf5e44adeb77e4d16e0d59c1f6113587c8653
Michael Rees Pullen <mrpullen@outlook.com>
2017-01-19 10:57:33 -0500

    Commit with two new provisioning cmdlets, passing unit tests.

---
+++ Commands/Provisioning/AddDataRowsToProvisioningTemplate.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.PowerShell.Commands.Provisioning
+{
+    [Cmdlet("Add", "PnPDataRowsToProvisioningTemplate")]
+    
+    [CmdletHelp("Adds datarows to a List inside of  an in-memory PnP Provisioning Template",
+        Category = CmdletHelpCategory.Provisioning)]
+    [CmdletExample(
+       Code = @"PS:> Add-PnPDataRowsToProvisioningTemplate -Path template.pnp -List 'PnPTestList' -Query '<View></View>' -Fields 'Title','Choice'",
+       Remarks = "Adds datarows to a list in an in-memory PnP Provisioning Template",
+       SortOrder = 1)]
+    [CmdletExample(
+       Code = @"PS:> Add-PnPDataRowsToProvisioningTemplate -Path template.pnp -List 'PnPTestList' -Query '<View></View>' -Fields 'Title','Choice' -IncludeSecurity",
+      Remarks = "Adds datarows to a list in an in-memory PnP Provisioning Template",
+       SortOrder = 2)]
+    public class AddDataRowsToProvisioningTemplate : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "Filename of the .PNP Open XML provisioning template to read from, optionally including full path.")]
+        public string Path;
+
+        [Parameter(Mandatory = true, HelpMessage = "The list to query")]
+        public ListPipeBind List;
+
+        [Parameter(Mandatory = true, HelpMessage = "The CAML query to execute against the list")]
+        public string Query;
+
+        [Parameter(Mandatory = true, HelpMessage = "The fields to retrieve. If not specified all fields will be loaded in the returned list object.")]
+        public string[] Fields;
+
+        [Parameter(Mandatory = false, Position = 5, HelpMessage = "The target Folder for the file to add to the in-memory template.")]
+        public SwitchParameter IncludeSecurity;
+
+        [Parameter(Mandatory = false, Position = 4, HelpMessage = "Allows you to specify ITemplateProviderExtension to execute while loading the template." )]
+        public ITemplateProviderExtension[] TemplateProviderExtensions;
+
+    
+
+        protected override void ExecuteCmdlet()
+        {
+
+            
+            var template = LoadProvisioningTemplate
+                    .LoadProvisioningTemplateFromFile(Path,
+                    SessionState.Path.CurrentFileSystemLocation.Path,
+                    TemplateProviderExtensions);
+            
+            if (template == null)
+            {
+                throw new ApplicationException("Invalid template file!");
+            }
+    //We will remove a list if it's found so we can get the list
+
+            ListInstance listInstance = template.Lists.Find(l => l.Title == List.Title);
+            if (listInstance == null)
+            {
+                throw new ApplicationException("List does not exist in the template file!");
+            }
+            
+            List spList = List.GetList(SelectedWeb);
+            ClientContext.Load(spList, l => l.RootFolder, l => l.HasUniqueRoleAssignments);
+            ClientContext.ExecuteQueryRetry();
+
+            CamlQuery query = new CamlQuery();
+            var viewFieldsStringBuilder = new StringBuilder();
+
+            viewFieldsStringBuilder.Append("<ViewFields>");
+            foreach (var field in Fields)
+            {
+                viewFieldsStringBuilder.AppendFormat("<FieldRef Name='{0}'/>", field);
+            }
+            viewFieldsStringBuilder.Append("</ViewFields>");
+            query.ViewXml = string.Format("<View>{0}{1}</View>", Query, viewFieldsStringBuilder);
+            var listItems = spList.GetItems(query);
+            ClientContext.Load(listItems, lI=>lI.Include(l=>l.HasUniqueRoleAssignments, l=>l.ContentType.StringId));
+            ClientContext.ExecuteQueryRetry();
+
+            Microsoft.SharePoint.Client.FieldCollection fieldCollection = spList.Fields;
+            ClientContext.Load(fieldCollection, fs => fs.Include(f => f.InternalName, f => f.FieldTypeKind));
+            ClientContext.ExecuteQueryRetry();
+
+
+            var rows = new DataRowCollection(template);
+            foreach(var listItem in listItems)
+            {
+                //Make sure we don't pull Folders.. Of course this won't work
+                if (listItem.ServerObjectIsNull == false) {
+
+                    ClientContext.Load(listItem);
+                    ClientContext.ExecuteQueryRetry();
+                    if (!(listItem.FileSystemObjectType == FileSystemObjectType.Folder))
+                    {
+
+
+
+                        DataRow row = new DataRow();
+                        if (IncludeSecurity && listItem.HasUniqueRoleAssignments)
+                        {
+                            row.Security.ClearSubscopes = true;
+                            row.Security.CopyRoleAssignments = false;
+
+                            var RoleAssignments = listItem.RoleAssignments;
+                            ClientContext.Load(RoleAssignments);
+                            ClientContext.ExecuteQueryRetry();
+
+
+
+                            ClientContext.Load(RoleAssignments, r => r.Include(a => a.Member.LoginName, a => a.Member, a => a.RoleDefinitionBindings));
+                            ClientContext.ExecuteQueryRetry();
+
+                            foreach (var roleAssignment in RoleAssignments)
+                            {
+                                var principalName = roleAssignment.Member.LoginName;
+                                var roleBindings = roleAssignment.RoleDefinitionBindings;
+                                foreach (var roleBinding in roleBindings)
+                                {
+                                    row.Security.RoleAssignments.Add(new OfficeDevPnP.Core.Framework.Provisioning.Model.RoleAssignment() { Principal = principalName, RoleDefinition = roleBinding.Name });
+                                }
+                            }
+                        }
+
+                        foreach (var field in Fields)
+                        {
+                            Microsoft.SharePoint.Client.Field dataField = fieldCollection.FirstOrDefault(f => f.InternalName == field);
+
+                            if (dataField != null)
+                            {
+                                var defaultFieldValue = listItem[field] as string;
+                                row.Values.Add(field, defaultFieldValue);
+
+                            }
+
+
+                        }
+
+
+
+
+                        rows.Add(row);
+                    }
+                }
+            }
+                template.Lists.Remove(listInstance);
+                listInstance.DataRows.AddRange(rows);
+                template.Lists.Add(listInstance);
+
+               
+                    // Determine the output file name and path
+                    var outFileName = System.IO.Path.GetFileName(Path);
+                    var outPath = new System.IO.FileInfo(Path).DirectoryName;
+                   
+                    // Save the template back to the storage
+                    var fileSystemConnector = new FileSystemConnector(outPath, "");
+                    var formatter = XMLPnPSchemaFormatter.LatestFormatter;
+
+                    XMLTemplateProvider provider = new XMLOpenXMLTemplateProvider(
+                          Path, fileSystemConnector);
+                    var templateFileName = outFileName.Substring(0, outFileName.LastIndexOf(".", StringComparison.Ordinal)) + ".xml";
+
+                    provider.SaveAs(template, templateFileName, formatter, TemplateProviderExtensions);
+
+                WriteObject(template);
+               
+        }
+
+      
+
+       
+       
+    }
+}
