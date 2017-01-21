2276130bf5d4c9ca8226797bd2daedc9e45d80c5
Mikael Svenson <miksvenson@gmail.com>
2017-01-21 22:19:32 +0100

    Added support for copying cross site collection

--- Commands/Files/CopyFile.cs
+++ Commands/Files/CopyFile.cs
@@ -1,8 +1,12 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Linq.Expressions;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
 using Microsoft.SharePoint.Client;
 using SharePointPnP.PowerShell.CmdletHelpAttributes;
 using Resources = SharePointPnP.PowerShell.Commands.Properties.Resources;
 using OfficeDevPnP.Core.Utilities;
+using File = Microsoft.SharePoint.Client.File;
 
 namespace SharePointPnP.PowerShell.Commands.Files
 {
@@ -12,15 +16,15 @@ namespace SharePointPnP.PowerShell.Commands.Files
         Category = CmdletHelpCategory.Files)]
     [CmdletExample(
         Remarks = "Copies a file named company.docx located in the document library called Documents located in the projects sitecollection under the managed path sites to the site collection otherproject located in the managed path sites. If a file named company.aspx already exists, it won't perform the copy.",
-        Code = @"PS:>Move-PnPFile -ServerRelativeUrl /sites/project/Documents/company.docx -TargetUrl /sites/otherproject/Documents/company.docx",
+        Code = @"PS:>Copy-PnPFile -ServerRelativeUrl /sites/project/Documents/company.docx -TargetUrl /sites/otherproject/Documents/company.docx",
         SortOrder = 1)]
     [CmdletExample(
         Remarks = "Copies a file named company.docx located in the document library called Documents located in the current site to the Documents library in the site collection otherproject located in the managed path sites. If a file named company.aspx already exists, it won't perform the copy.",
-        Code = @"PS:>Move-PnPFile -SiteRelativeUrl Documents/company.aspx -TargetUrl /sites/otherproject/Documents/company.docx",
+        Code = @"PS:>Copy-PnPFile -SiteRelativeUrl Documents/company.aspx -TargetUrl /sites/otherproject/Documents/company.docx",
         SortOrder = 2)]
     [CmdletExample(
         Remarks = "Copies a file named company.docx located in the document library called Documents located in the projects sitecollection under the managed path sites to the site collection otherproject located in the managed path sites. If a file named company.aspx already exists, it will still perform the copy and replace the original company.aspx file.",
-        Code = @"PS:>Move-PnPFile -ServerRelativeUrl /sites/project/Documents/company.docx -TargetUrl /sites/otherproject/Documents/company.docx -OverwriteIfAlreadyExists",
+        Code = @"PS:>Copy-PnPFile -ServerRelativeUrl /sites/project/Documents/company.docx -TargetUrl /sites/otherproject/Documents/company.docx -OverwriteIfAlreadyExists",
         SortOrder = 3)]
 
     public class CopyFile : SPOWebCmdlet
@@ -40,6 +44,9 @@ namespace SharePointPnP.PowerShell.Commands.Files
         [Parameter(Mandatory = false, HelpMessage = "If provided, no confirmation will be requested and the action will be performed")]
         public SwitchParameter Force;
 
+        [Parameter(Mandatory = false, HelpMessage = "If provided, the source folder name will not be created, only the contents within it.")]
+        public SwitchParameter SkipSourceFolder;
+
         protected override void ExecuteCmdlet()
         {
             if (ParameterSetName == "SITE")
@@ -49,17 +56,97 @@ namespace SharePointPnP.PowerShell.Commands.Files
             }
 
             var file = SelectedWeb.GetFileByServerRelativeUrl(ServerRelativeUrl);
-
-            ClientContext.Load(file, f => f.Name);
+            var folder = SelectedWeb.GetFolderByServerRelativeUrl(ServerRelativeUrl);
+            ClientContext.Load(file, f => f.Name, f => f.Exists);
             ClientContext.ExecuteQueryRetry();
+            ClientContext.Load(folder, f => f.Name, f => f.Exists);
+            ClientContext.ExecuteQueryRetry();
+            bool srcIsFolder = folder.Exists;
 
             if (Force || ShouldContinue(string.Format(Resources.CopyFile0To1, ServerRelativeUrl, TargetUrl), Resources.Confirm))
             {
-                file.CopyTo(TargetUrl, OverwriteIfAlreadyExists);
-
+                Uri uri = new Uri(ClientContext.Url);
+                Uri targetUri = new Uri(uri, TargetUrl);
+                var webUrl = Microsoft.SharePoint.Client.Web.WebUrlFromFolderUrlDirect(ClientContext, targetUri);
+                var srcSite = ClientContext.Site;
+                ClientContext.Load(srcSite, s => s.Url);
                 ClientContext.ExecuteQueryRetry();
+
+                var targetContext = ClientContext.Clone(webUrl.AbsoluteUri);
+                var dstSite = targetContext.Site;
+                targetContext.Load(dstSite, s => s.Url);
+                targetContext.ExecuteQueryRetry();
+                if (srcSite.Url == dstSite.Url)
+                {
+                    //same site collection
+                    file.CopyTo(TargetUrl, OverwriteIfAlreadyExists);
+                    ClientContext.ExecuteQueryRetry();
+                }
+                else
+                {
+                    //different site collections
+                    Folder targetFolder = null;
+                    string fileOrFolderName = null;
+                    try
+                    {
+                        targetFolder = targetContext.Web.GetFolderByServerRelativeUrl(TargetUrl);
+                        targetContext.Load(targetFolder, f => f.Name, f => f.Exists);
+                        targetContext.ExecuteQueryRetry();
+                    }
+                    catch (Exception)
+                    {
+                        Expression<Func<List, object>> expressionRelativeUrl = l => l.RootFolder.ServerRelativeUrl;
+                        var query = targetContext.Web.Lists.IncludeWithDefaultProperties(expressionRelativeUrl);
+                        var lists = targetContext.LoadQuery(query);
+                        targetContext.ExecuteQueryRetry();
+                        foreach (List targetList in lists)
+                        {
+                            if (!TargetUrl.StartsWith(targetList.RootFolder.ServerRelativeUrl, StringComparison.InvariantCultureIgnoreCase)) continue;
+                            fileOrFolderName = Regex.Replace(TargetUrl, targetList.RootFolder.ServerRelativeUrl, "", RegexOptions.IgnoreCase).Trim('/');
+                            targetFolder = srcIsFolder ? targetList.RootFolder.EnsureFolder(fileOrFolderName) : targetList.RootFolder;
+                            break;
+                        }
+                    }
+                    if (targetFolder == null) throw new Exception("Target does not exist");
+                    if (srcIsFolder)
+                    {
+                        if (!SkipSourceFolder)
+                        {
+                            targetFolder = targetFolder.EnsureFolder(folder.Name);
+                        }
+                        CopyFolder(folder, targetFolder);
+                    }
+                    else
+                    {
+                        UploadFile(file, targetFolder, fileOrFolderName);
+                    }
+                }
             }
+        }
+
+        private void CopyFolder(Folder sourceFolder, Folder targetFolder)
+        {
+            ClientContext.Load(sourceFolder, f => f.Files, f => f.Folders);
+            ClientContext.ExecuteQueryRetry();
+
+            foreach (File file in sourceFolder.Files)
+            {
+                UploadFile(file, targetFolder);
+            }
+            foreach (Folder folder in sourceFolder.Folders)
+            {
+                var childFolder = targetFolder.EnsureFolder(folder.Name);
+                CopyFolder(folder, childFolder);
+            }
+        }
+
+        private void UploadFile(File srcFile, Folder targetFolder, string filename = "")
+        {
+            var binaryStream = srcFile.OpenBinaryStream();
+            ClientContext.ExecuteQueryRetry();
+            if (string.IsNullOrWhiteSpace(filename)) filename = srcFile.Name;
+            targetFolder.UploadFile(filename, binaryStream.Value, OverwriteIfAlreadyExists);
+            targetFolder.Context.ExecuteQueryRetry();
         }
     }
 }
- 
