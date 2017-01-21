fa934a88c5383774afe65e780ba16710366470b6
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-01-21 03:49:35 +0100

    Refactored in a more generic approach

--- Commands/Lists/GetList.cs
+++ Commands/Lists/GetList.cs
@@ -27,56 +27,37 @@ namespace SharePointPnP.PowerShell.Commands.Lists
         Code = "PS:> Get-PnPList -Identity Lists/Announcements",
         Remarks = "Returns a list with the given url.",
         SortOrder = 3)]
-    public class GetList : SPOWebCmdlet
+    public class GetList : PnPWebRetrievalCmdlet<List>
     {
         [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 0, HelpMessage = "The ID, name or Url (Lists/MyList) of the list.")]
         public ListPipeBind Identity;
 
-        [Parameter(Mandatory = false, ValueFromPipeline = false, Position = 1, HelpMessage = "Additional Properties to retrieve for the List item(s) to be returned")]  
-        [ValidateSet("AllowDeletion","BrowserFileHandling","ContentTypes","DataSource","DefaultDisplayFormUrl","DefaultEditFormUrl","DefaultNewFormUrl","DefaultViewPath","DocumentTemplateUrl","EffectiveBasePermissions","EffectiveBasePermissionsForUI","EnableAssignToEmail","EventReceivers","ExcludeFromOfflineClient","Fields","Forms","HasUniqueRoleAssignments","IsSiteAssetsLibrary","IsSystemList","ReadSecurity","RoleAssignments","SchemaXml","Tag","UserCustomActions","ValidationFormula","ValidationMessage","Views","WorkflowAssociations","WriteSecurity")]
-        public string[] Includes;
+        //[Parameter(Mandatory = false, ValueFromPipeline = false, Position = 1, HelpMessage = "Additional Properties to retrieve for the List item(s) to be returned")]  
+        //[ValidateSet("AllowDeletion","BrowserFileHandling","ContentTypes","DataSource","DefaultDisplayFormUrl","DefaultEditFormUrl","DefaultNewFormUrl","DefaultViewPath","DocumentTemplateUrl","EffectiveBasePermissions","EffectiveBasePermissionsForUI","EnableAssignToEmail","EventReceivers","ExcludeFromOfflineClient","Fields","Forms","HasUniqueRoleAssignments","IsSiteAssetsLibrary","IsSystemList","ReadSecurity","RoleAssignments","SchemaXml","Tag","UserCustomActions","ValidationFormula","ValidationMessage","Views","WorkflowAssociations","WriteSecurity")]
+        //public string[] Includes;
 
         protected override void ExecuteCmdlet()
         {
+
+            AlwaysLoadProperties = new[] { "Id", "BaseTemplate", "OnQuickLaunch", "DefaultViewUrl", "Title", "Hidden" };
+
+            var expressions = Expressions.ToList();
+            Expression<Func<List, object>> expressionRelativeUrl = l => l.RootFolder.ServerRelativeUrl;
+
+            expressions.Add(expressionRelativeUrl);
+
             if (Identity != null)
             {
                 var list = Identity.GetList(SelectedWeb);
-                if (Includes != null && Includes.Length > 0)
-                {
-                    List<string> fields = new List<string>();
-                    fields.AddRange(Includes);
-                    List<Expression<Func<List, object>>> expressions = GetPropertyExpression<List>(fields);
-                    
-                    
-                    ClientContext.Load(list, expressions.ToArray());
-                    ClientContext.ExecuteQueryRetry();
-                }
 
+                list.EnsureProperties(expressions.ToArray());
 
                 WriteObject(list);
 
             }
             else
             {
-                string [] defaultFields = { "Id", "BaseTemplate", "OnQuickLaunch", "DefaultViewUrl", "Title", "Hidden" };
-
-                List<string> fields = new List<string>();
-                fields.AddRange(defaultFields);
-
-                if (Includes != null && Includes.Length > 0)
-                {
-                    fields.AddRange(Includes);
-                  //  fields.Add("RootFolder.ServerRelativeUrl");
-                }
-                
-                List<Expression<Func<List, object>>> expressions = GetPropertyExpression<List>(fields);
-                
-                //Now handle that special case of  RootFolder.ServerRelativeUrl -- Should really do a check for "sub members and implement this recursively...
-                Expression<Func<List, object>> expressionRelativeUrl = l => l.RootFolder.ServerRelativeUrl;
-                expressions.Add(expressionRelativeUrl);
-
                 var query = (SelectedWeb.Lists.IncludeWithDefaultProperties(expressions.ToArray()));
-               
                 var lists = ClientContext.LoadQuery(query);
                 ClientContext.ExecuteQueryRetry();
                 WriteObject(lists, true);
@@ -90,12 +71,12 @@ namespace SharePointPnP.PowerShell.Commands.Lists
 
             var properties = type.GetProperties();
 
-            foreach(var Include in Includes)
+            foreach (var Include in Includes)
             {
                 //Add . checking for nested includes.. then it would call this T function recursively suing the type of the property
                 // example RootFolder.ServerRelativeUrl GetPropertyExpression<Folder>(new string [] { "ServerRelativeUrl" }
                 var sanityCheck = (from property in properties where property.Name == Include select property).FirstOrDefault();
-                if(sanityCheck != null)
+                if (sanityCheck != null)
                 {
                     //the property exists in the parent type.
 
@@ -103,7 +84,7 @@ namespace SharePointPnP.PowerShell.Commands.Lists
                     MemberExpression propertyExpression = Expression.Property(paramExpression, Include);
                     var bodyExpression = Expression.Convert(propertyExpression, typeof(Object));
                     //MemberExpression propertyExpression = Expression.Property(instance,  Include);
-                    var expression = System.Linq.Expressions.ParameterExpression.Lambda<Func<T, object>>(bodyExpression, new[] { paramExpression });
+                    var expression = Expression.Lambda<Func<T, object>>(bodyExpression, new[] { paramExpression });
                     expressions.Add(expression);
                 }
             }
