e1889b79c66111500745e9b98ef595e6bfb00284
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-01-22 19:11:20 +0100

    Cleanup

--- Commands/Base/PipeBinds/AppPipeBind.cs
+++ Commands/Base/PipeBinds/AppPipeBind.cs
@@ -14,7 +14,7 @@ namespace SharePointPnP.PowerShell.Commands.Base.PipeBinds
             _appInstance = instance;
         }
 
-      
+
         public AppPipeBind(Guid guid)
         {
             _id = guid;
@@ -22,24 +22,18 @@ namespace SharePointPnP.PowerShell.Commands.Base.PipeBinds
 
         public AppPipeBind(string id)
         {
-            if(!Guid.TryParse(id, out _id))
+            if (!Guid.TryParse(id, out _id))
             {
                 _title = id;
             }
-            
+
         }
 
         public Guid Id => _id;
 
-        public string Title
-        {
-            get { return _title; }
-        }
+        public string Title => _title;
 
-        public AppInstance Instance
-        {
-            get { return _appInstance; }
-        }
+        public AppInstance Instance => _appInstance;
 
         internal AppInstance GetAppInstance(Web web)
         {
@@ -49,32 +43,28 @@ namespace SharePointPnP.PowerShell.Commands.Base.PipeBinds
                 instance = Instance;
                 return instance;
             }
-            else if (Id != Guid.Empty)
+            if (Id != Guid.Empty)
             {
                 instance = web.GetAppInstanceById(Id);
                 web.Context.Load(instance);
                 web.Context.ExecuteQueryRetry();
                 return instance;
             }
-            else if (!string.IsNullOrEmpty(Title))
+            if (!string.IsNullOrEmpty(Title))
             {
-                
+
                 var instances = web.GetAppInstances();
                 web.Context.Load(instances);
                 web.Context.ExecuteQueryRetry();
-                foreach(var inst in instances)
+                foreach (var inst in instances)
                 {
-                    if(inst.Title == Title)
+                    if (inst.Title == Title)
                     {
                         return inst;
                     }
                 }
             }
-            
-                return null;
-            
+            return null;
         }
-
-
     }
 }
