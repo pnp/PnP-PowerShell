4200f871fc4827c1a12f16e6652094dc81a76793
Mikael Svenson <miksvenson@gmail.com>
2017-02-16 23:05:17 +0100

    Added support for appid/appsecret

--- Commands/Base/ConnectPnPMicrosoftGraph.cs
+++ Commands/Base/ConnectPnPMicrosoftGraph.cs
@@ -14,20 +14,25 @@ namespace SharePointPnP.PowerShell.Commands.Base
        Code = "PS:> Connect-PnPMicrosoftGraph -Scopes $arrayOfScopes",
        Remarks = "Connects to Azure AD and gets and OAuth 2.0 Access Token to consume the Microsoft Graph API including the declared permission scopes. The available permission scopes are defined at the following URL: https://graph.microsoft.io/en-us/docs/authorization/permission_scopes",
        SortOrder = 1)]
+    [CmdletExample(
+       Code = "PS:> Connect-PnPMicrosoftGraph -AppId '<id>' -AppSecret '<key>' -AADDomain 'contoso.onmicrosoft.com'",
+       Remarks = "Connects to Azure AD and gets and OAuth 2.0 Access Token to consume the Microsoft Graph API via an app's declared permission scopes registred at the following URL: https://apps.dev.microsoft.com",
+       SortOrder = 2)]
     public class ConnectPnPMicrosoftGraph : PSCmdlet
     {
         private const string MSALPnPPowerShellClientId = "bb0c5778-9d5c-41ea-a4a8-8cd417b3ab71";
         private const string RedirectUri = "urn:ietf:wg:oauth:2.0:oob";
         private static readonly Uri AADLogin = new Uri("https://login.microsoftonline.com/");
-        private static readonly string[] DefaultScope = new[] { "https://graph.microsoft.com/.default" };
+        private static readonly string[] DefaultScope = { "https://graph.microsoft.com/.default" };
 
         [Parameter(Mandatory = true, HelpMessage = "The array of permission scopes for the Microsoft Graph API.", ParameterSetName = "Scope")]
-        public String[] Scopes;
+        public string[] Scopes;
 
         [Parameter(Mandatory = true, HelpMessage = "The client id of the app which gives you access to the Microsoft Graph API.", ParameterSetName = "AAD")]
-        public string ClientId;
+        public string AppId;
+
         [Parameter(Mandatory = true, HelpMessage = "The app key of the app which gives you access to the Microsoft Graph API.", ParameterSetName = "AAD")]
-        public string AppKey;
+        public string AppSecret;
 
         [Parameter(Mandatory = true, HelpMessage = "The AAD where the O365 app is registred. Eg.: contoso.com, or contoso.onmicrosoft.com.", ParameterSetName = "AAD")]
         public string AADDomain;
@@ -37,15 +42,15 @@ namespace SharePointPnP.PowerShell.Commands.Base
             AuthenticationResult authenticationResult;
             if (Scopes != null)
             {
-                PublicClientApplication clientApplication = new PublicClientApplication(MSALPnPPowerShellClientId);
+                var clientApplication = new PublicClientApplication(MSALPnPPowerShellClientId);
                 // Acquire an access token for the given scope
                 authenticationResult = clientApplication.AcquireTokenAsync(Scopes).GetAwaiter().GetResult();
             }
             else
             {
-                var credz = new ClientCredential(AppKey);
+                var appCredentials = new ClientCredential(AppSecret);
                 var authority = new Uri(AADLogin, AADDomain).AbsoluteUri;
-                ConfidentialClientApplication clientApplication = new ConfidentialClientApplication(authority, ClientId, RedirectUri, credz, null);
+                var clientApplication = new ConfidentialClientApplication(authority, AppId, RedirectUri, appCredentials, null);
                 authenticationResult = clientApplication.AcquireTokenForClient(DefaultScope, null).GetAwaiter().GetResult();
             }
 
