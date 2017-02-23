14275c0785a55424d24ee69fc0d5d73fdb344952
Mikael Svenson <miksvenson@gmail.com>
2017-02-23 15:37:52 +0100

    Changed help text and added samples.

--- Commands/Graph/SetUnifiedGroup.cs
+++ Commands/Graph/SetUnifiedGroup.cs
@@ -1,16 +1,11 @@
 ﻿using OfficeDevPnP.Core.Entities;
 using OfficeDevPnP.Core.Framework.Graph;
-using OfficeDevPnP.Core.Utilities;
 using SharePointPnP.PowerShell.CmdletHelpAttributes;
 using SharePointPnP.PowerShell.Commands.Base;
 using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharePointPnP.PowerShell.Commands.Graph
 {
@@ -29,6 +24,14 @@ namespace SharePointPnP.PowerShell.Commands.Graph
        Code = @"PS:> Set-PnPUnifiedGroup -Identity $group -GroupLogoPath "".\MyLogo.png""",
        Remarks = "Sets a specific Office 365 Group logo.",
        SortOrder = 3)]
+    [CmdletExample(
+       Code = @"PS:> Set-PnPUnifiedGroup -Identity $group -IsPrivate:$false",
+       Remarks = "Sets a group to be Public if previously Private.",
+       SortOrder = 4)]
+    [CmdletExample(
+       Code = @"PS:> Set-PnPUnifiedGroup -Identity $group -Owners demo@contoso.com",
+       Remarks = "Adds demo@contoso.com as an additional owner to the group.",
+       SortOrder = 5)]
     public class SetUnifiedGroup : PnPGraphCmdlet
     {
         [Parameter(Mandatory = true, HelpMessage = "The Identity of the Office 365 Group.", ValueFromPipeline = true)]
@@ -40,10 +43,10 @@ namespace SharePointPnP.PowerShell.Commands.Graph
         [Parameter(Mandatory = false, HelpMessage = "The Description of the group to set.")]
         public string Description;
 
-        [Parameter(Mandatory = false, HelpMessage = "The array UPN values of the group's owners.")]
+        [Parameter(Mandatory = false, HelpMessage = "The array UPN values of owners to add to the group.")]
         public String[] Owners;
 
-        [Parameter(Mandatory = false, HelpMessage = "The array UPN values of the group's members.")]
+        [Parameter(Mandatory = false, HelpMessage = "The array UPN values of members to add to the group.")]
         public String[] Members;
 
         [Parameter(Mandatory = false, HelpMessage = "Makes the group private when selected.")]
