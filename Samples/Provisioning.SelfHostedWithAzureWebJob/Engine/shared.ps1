d2c9afdc3828cc49cf4689133d305bd0343b9009
Mikael Svenson <miksvenson@gmail.com>
2017-03-23 10:55:17 +0100

    Dynamic loading of posh from nuget

--- Samples/Provisioning.SelfHostedWithAzureWebJob/Engine/shared.ps1
+++ Samples/Provisioning.SelfHostedWithAzureWebJob/Engine/shared.ps1
@@ -1,5 +1,16 @@
 $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
+
+if (-not (Test-Path $PSScriptRoot\bundle\SharePointPnPPowerShellOnline.psd1)) {
+    Write-Host "a" -ForegroundColor Yellow
+    Set-Alias nuget "$PSScriptRoot\nuget.exe"
+    nuget install SharePointPnPPowerShellOnline -configFile $PSScriptRoot\nuget.config -OutputDirectory $PSScriptRoot\bundle\
+
+    $item = Get-ChildItem -Path .\Engine\bundle\ -Filter "SharePointPnPPowerShellOnline*"
+    Move-Item -Path "$($item.FullName)\*" -Destination .\Engine\bundle\
+    $item | Remove-Item
+}
+
 Add-Type -Path $PSScriptRoot\bundle\Microsoft.SharePoint.Client.Taxonomy.dll -ErrorAction SilentlyContinue
 Add-Type -Path $PSScriptRoot\bundle\Microsoft.SharePoint.Client.DocumentManagement.dll -ErrorAction SilentlyContinue
 Add-Type -Path $PSScriptRoot\bundle\Microsoft.SharePoint.Client.WorkflowServices.dll -ErrorAction SilentlyContinue
@@ -12,9 +23,9 @@ Import-Module $PSScriptRoot\bundle\SharePointPnPPowerShellOnline.psd1 -ErrorActi
 Set-PnPTraceLog -Off
 
 $tenantURL = ([environment]::GetEnvironmentVariable("APPSETTING_TenantURL"))
-if(!$tenantURL){
+if (!$tenantURL) {
     $tenant = ([environment]::GetEnvironmentVariable("APPSETTING_Tenant"))
-    $tenantURL = [string]::format("https://{0}.sharepoint.com",$tenant)
+    $tenantURL = [string]::format("https://{0}.sharepoint.com", $tenant)
 }
 
 $primarySiteCollectionAdmin = ([environment]::GetEnvironmentVariable("APPSETTING_PrimarySiteCollectionOwnerEmail"))
@@ -29,12 +40,12 @@ $Global:lastContextUrl = ''
 
 #Azure appsettings variables - remove prefix when adding in azure
 $appId = ([environment]::GetEnvironmentVariable("APPSETTING_AppId"))
-if(!$appId){
+if (!$appId) {
     $appId = ([environment]::GetEnvironmentVariable("APPSETTING_ClientId"))
 }
 
 $appSecret = ([environment]::GetEnvironmentVariable("APPSETTING_AppSecret"))
-if(!$appSecret){
+if (!$appSecret) {
     $appSecret = ([environment]::GetEnvironmentVariable("APPSETTING_ClientSecret"))
 }
 
@@ -43,36 +54,37 @@ $tenantUrl = $uri.Scheme + "://" + $uri.Host
 $tenantAdminUrl = $tenantUrl.Replace(".sharepoint", "-admin.sharepoint")
 
 
-function Connect([string]$Url){    
-    if($Url -eq $Global:lastContextUrl){
+function Connect([string]$Url) {    
+    if ($Url -eq $Global:lastContextUrl) {
         return
     }
     if ($appId -ne $null -and $appSecret -ne $null) {
         Connect-PnPOnline -Url $Url -AppId $appId -AppSecret $appSecret
-    } else {
+    }
+    else {
         Connect-PnPOnline -Url $Url
     }
     $Global:lastContextUrl = $Url
 }
 
-function GetMailContent{
+function GetMailContent {
     Param(
         [string]$email,
         [string]$mailFile
     )
     $ext = "en";
-    if($mail) {
-        $ext = $email.Substring($email.LastIndexOf(".")+1)
+    if ($mail) {
+        $ext = $email.Substring($email.LastIndexOf(".") + 1)
     }
     $filename = "$PSScriptRoot/resources/$mailFile-mail-$ext.txt"
-    if(-not (Test-Path $filename)) {
+    if (-not (Test-Path $filename)) {
         $ext = "en"
         $filename = "$PSScriptRoot/resources/$mailFile-mail-$ext.txt"
     }
     return ([IO.File]::ReadAllText($filename)).Split("|")
 }
 
-function GetLoginName{
+function GetLoginName {
     Param(
         [int]$lookupId
     )
@@ -85,13 +97,13 @@ function GetLoginName{
 function SetRequestAccessEmail([string]$siteUrl, [string]$ownersEmail) {
     Connect -Url $siteUrl
     $emails = Get-PnPRequestAccessEmails
-    if($emails -ne $ownersEmail) {
+    if ($emails -ne $ownersEmail) {
         Write-Output "`tSetting site request e-mail to $ownersEmail"    
         Set-PnPRequestAccessEmails -Emails $ownersEmail
     }
 }
 
-function SyncPermissions{
+function SyncPermissions {
     Param(
         [string]$siteUrl,
         [Microsoft.SharePoint.Client.ListItem]$item
@@ -109,9 +121,9 @@ function SyncPermissions{
 
     Connect -Url "$tenantURL$siteDirectorySiteUrl"
 
-    $owners = @($owners -notlike 'SHAREPOINT\system' |% {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
-    $members = @($members -notlike 'SHAREPOINT\system' |% {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
-    $visitors = @($visitors -notlike 'SHAREPOINT\system' |% {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
+    $owners = @($owners -notlike 'SHAREPOINT\system' | % {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
+    $members = @($members -notlike 'SHAREPOINT\system' | % {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
+    $visitors = @($visitors -notlike 'SHAREPOINT\system' | % {New-PnPUser -LoginName $_ | select -ExpandProperty ID} | sort) 
     
     $existingOwners = @($item["$($columnPrefix)SiteOwners"] | select -ExpandProperty LookupId | sort)
     $existingMembers = @($item["$($columnPrefix)SiteMembers"] | select -ExpandProperty LookupId | sort)
@@ -121,7 +133,7 @@ function SyncPermissions{
     $diffMember = Compare-Object -ReferenceObject $members -DifferenceObject $existingMembers -PassThru
     $diffVisitor = Compare-Object -ReferenceObject $visitors -DifferenceObject $existingVisitors -PassThru
 
-    if($diffOwner -or $diffMember -or $diffVisitor) {
+    if ($diffOwner -or $diffMember -or $diffVisitor) {
         Write-Host "`tUpdating changed owners/members/visitors"
         $siteItem = Set-PnPListItem -List $siteDirectoryList -Identity $itemId -Values @{"$($columnPrefix)SiteOwners" = $owners; "$($columnPrefix)SiteMembers" = $members; "$($columnPrefix)SiteVisitors" = $visitors}
     }
