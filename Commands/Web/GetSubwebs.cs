78ddede07519f0028b43a04c6ee79f0bc0115e5a
Koen Zomers <koen@zomers.eu>
2017-09-04 00:28:45 +0200

    Implementing PnPWebRetrievalsCmdlet

--- Commands/Web/GetSubwebs.cs
+++ Commands/Web/GetSubwebs.cs
@@ -1,18 +1,19 @@
 ﻿using System.Management.Automation;
 using Microsoft.SharePoint.Client;
-using web = Microsoft.SharePoint.Client.Web;
 using SharePointPnP.PowerShell.CmdletHelpAttributes;
 using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
 
 namespace SharePointPnP.PowerShell.Commands
 {
     [Cmdlet(VerbsCommon.Get, "PnPSubWebs")]
     [CmdletHelp("Returns the subwebs of the current web", 
         Category = CmdletHelpCategory.Webs,
-        OutputType = typeof(List<web>),
+        OutputType = typeof(WebCollection),
         OutputTypeLink = "https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.web.aspx")]
-    public class GetSubWebs : PnPWebCmdlet
+    public class GetSubWebs : PnPWebRetrievalsCmdlet<WebCollection>
     {
         [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 0)]
         public WebPipeBind Identity;
@@ -22,35 +23,36 @@ namespace SharePointPnP.PowerShell.Commands
 
         protected override void ExecuteCmdlet()
         {
-            var webs = SelectedWeb.Context.LoadQuery(SelectedWeb.Webs);
-            SelectedWeb.Context.ExecuteQueryRetry();
+            DefaultRetrievalExpressions = new Expression<Func<WebCollection, object>>[] { wc => wc.Include(w => w.Id, w => w.Url, w => w.Title, w => w.ServerRelativeUrl) };
+
+            SelectedWeb.Webs.EnsureProperties(RetrievalExpressions);
             if (!Recurse)
             {
-                WriteObject(webs, true);
+                WriteObject(SelectedWeb.Webs, true);
             }
-            else
-            {
-                var subwebs = new List<web>();
-                subwebs.AddRange(webs);
-                foreach (var subweb in webs)
-                {
-                    subwebs.AddRange(GetSubWebsInternal(subweb));
-                }
-                WriteObject(subwebs, true);
-            }
+        //    else
+        //    {
+        //        var subwebs = new List<web>();
+        //        subwebs.AddRange(webs);
+        //        foreach (var subweb in webs)
+        //        {
+        //            subwebs.AddRange(GetSubWebsInternal(subweb));
+        //        }
+        //        WriteObject(subwebs, true);
+        //    }
         }
 
-        private List<web> GetSubWebsInternal(web subweb)
-        {
-            var subwebs = new List<web>();
-            var webs = subweb.Context.LoadQuery(subweb.Webs);
-            subweb.Context.ExecuteQueryRetry();
-            subwebs.AddRange(webs);
-            foreach (var sw in webs)
-            {
-                subwebs.AddRange(GetSubWebsInternal(sw));
-            }
-            return subwebs;
-        }
+        //private List<web> GetSubWebsInternal(web subweb)
+        //{
+        //    var subwebs = new List<web>();
+        //    var webs = subweb.Context.LoadQuery(subweb.Webs);
+        //    subweb.Context.ExecuteQueryRetry();
+        //    subwebs.AddRange(webs);
+        //    foreach (var sw in webs)
+        //    {
+        //        subwebs.AddRange(GetSubWebsInternal(sw));
+        //    }
+        //    return subwebs;
+        //}
     }
 }
