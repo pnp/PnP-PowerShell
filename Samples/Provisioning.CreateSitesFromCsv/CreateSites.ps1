17ada180ee56a02bfc10297bc5bb1f22eead3bea
Velin Georgiev <velin.georgiev@gamil.com>
2017-09-20 00:31:07 +0100

    Adds SharePointPnPPowerShellOnline sample that creates SharePoint sites from csv file.

--- Samples/Provisioning.CreateSitesFromCsv/CreateSites.ps1
+++ Samples/Provisioning.CreateSitesFromCsv/CreateSites.ps1
@@ -1,18 +1,4 @@
-﻿# This script creates site collections and/or sub webs from CSV file configuration.
-# Please note: 
-# - Only sub webs one level/leaf deeper from the root site can be created. The script and the csv data have to be adjusted, if sub webs should be created on two or more levels/leafs away from the root web.
-# - The csv sites are created from top to bottom so sub webs to be created should be listed after site collections that do not exist.
-
-# Define variables.
-
-# The root directory for the sctipts and the csv file.
-$rootdir = "./"
-
-# The directory path for the csv file.
-$createSitesCsvPath = -join($rootdir, "CreateSites.csv")
-
-# User email and password stored in a txt files.
-$credentialsFilePath = -join($rootdir, "credentials.txt")
+﻿# This script creates site collections and/or sub webs from csv configuration template.
 
 # Change the policy settings if needed.
 #$policy = Get-ExecutionPolicy
@@ -20,79 +6,96 @@ $credentialsFilePath = -join($rootdir, "credentials.txt")
 #    Set-ExecutionPolicy RemoteSigned
 #}
 
-# Install and import the PnP SharePoint modules.
-if (-not (Get-Module -ListAvailable -Name SharePointPnPPowerShellOnline)) {
+if (-not (Get-Module -ListAvailable -Name SharePointPnPPowerShellOnline)) 
+{
     Install-Module SharePointPnPPowerShellOnline
 }
 
 Import-Module SharePointPnPPowerShellOnline
 
-# Add/Save user credentials to txt file.
-if(-not (Test-Path $credentialsFilePath)){
-    $user = Read-Host -Prompt "Please enter your email"
-    $pass = Read-host -AsSecureString | ConvertFrom-SecureString
-    "$user`n$pass" | Out-File $credentialsFilePath
+# Gets or Sets the csv data.
+$csvConfig = $null
+
+# Gets or Sets the tenant admin credentials.
+$credentials = $null
+
+# Windows Credentials Manager credential label.
+$winCredentialsManagerLabel = "SPFX"
+
+# Imports the csv configuration template.
+try 
+{ 
+    $csvConfig = Import-Csv -Path "./CreateSites.csv"
+} 
+catch 
+{
+    # Prompts for csv configuration template path.
+    $csvConfigPath = Read-Host -Prompt "Please enter the csv configuration template full path"
+    $csvConfig = Import-Csv -Path $csvConfigPath
 }
 
-# Get user credentials from txt file.
-$email = (Get-Content $credentialsFilePath)[0]
-$password = (Get-Content $credentialsFilePath)[1] | ConvertTo-SecureString
-$credentials = New-Object –TypeName "System.Management.Automation.PSCredential" –ArgumentList $email, $password
+# Gets stored credentials from the Windows Credential Manager or show prompt.
+# How to use windows credential manager:
+# https://github.com/SharePoint/PnP-PowerShell/wiki/How-to-use-the-Windows-Credential-Manager-to-ease-authentication-with-PnP-PowerShell
+if((Get-PnPStoredCredential -Name $winCredentialsManagerLabel) -ne $null)
+{
+    $credentials = $winCredentialsManagerLabel
+}
+else
+{
+    # Prompts for credentials, if not found in the Windows Credential Manager.
+    $email = Read-Host -Prompt "Please enter tenant admin email"
+    $pass = Read-host -AsSecureString "Please enter tenant admin password"
+    $credentials = New-Object –TypeName "System.Management.Automation.PSCredential" –ArgumentList $email, $pass
+}
 
-# Create site collections and sub sites based on the csv file config data.
-# Test comment
-$createSitesConfig = Import-Csv -Path $createSitesCsvPath
+if($credentials -eq $null -or $csvConfig -eq $null) 
+{
+    Write-Host "Error: Not enough details." -ForegroundColor DarkRed
+    exit 1
+}
 
-# Iterate over the csv rows and create site collections or sub sites depending on the 'Type' column from the csv file.
-foreach ($item in $createSitesConfig)
+# Iterates over the csv confuguration template rows and creates site collections or sub sites.
+foreach ($item in $csvConfig) 
 {
     $tenantUrl = $item.RootUrl -replace ".sharepoint.com.+", ".sharepoint.com"
     $siteUrl = -join($item.RootUrl, "/", $item.SiteUrl)
     
-    if ($item.Type -eq "SiteCollection")
+    if ($item.Type -eq "SiteCollection") 
     {
         Connect-PnPOnline $tenantUrl -Credentials $credentials
 
-        Write-Host "Connecting to $tenantUrl" -ForegroundColor Green
-
-        Write-Host "Provisioning SiteCollection $siteUrl" -ForegroundColor Yellow
-
-        if(Get-PnPTenantSite | where{$_.Url -eq $siteUrl}){
-            # Please note this will remove any existing site collection at the specified url.
-
-            Write-Host "$siteUrl exists. Removing the site collection." -ForegroundColor Yellow
-
-            Remove-PnPTenantSite -Url $siteUrl -Force
+        Write-Host "Provisioning site collection $siteUrl" -ForegroundColor Yellow
+         
+        if(Get-PnPTenantSite | where {$_.Url -eq $siteUrl}) 
+        {
+            Write-Host "Site collection $siteUrl exists. Moving to the next one." -ForegroundColor Yellow
+            continue
         }
 
-        # Create new Tenant Site Collection.
-        New-PnPTenantSite -Owner $item.Owner -TimeZone 0 -Title $item.Title -Url $siteUrl -Template $item.Template -Wait
+        # Creates new site collection.
+        New-PnPTenantSite -Owner $item.Owner -TimeZone $item.TimeZone -Title $item.Title -Url $siteUrl -Template $item.Template -Lcid $item.Locale -Wait
 
-        Write-Host "SiteCollection $siteUrl successfully created." -ForegroundColor Yellow
-    }
-
-
-    if ($item.Type -eq "SubWeb")
+        Write-Host "SiteCollection $siteUrl successfully created." -ForegroundColor Green
+    }  
+    elseif ($item.Type -eq "SubWeb") 
     {
+
         $siteCollectionUrl = $item.RootUrl
 
         Connect-PnPOnline $siteCollectionUrl -Credentials $credentials
 
-        Write-Host "Connecting to $siteCollectionUrl" -ForegroundColor Green
-
         Write-Host "Provisioning sub web $siteUrl." -ForegroundColor Yellow
         
-        if(Get-PnPSubWebs | where{$_.Url -eq $siteUrl}){
-            # Please note this will remove any existing sub web at the specified url.
-
-            Write-Host "$siteUrl exists. Removing the sub web." -ForegroundColor Yellow
-
-            Remove-PnPWeb -Url $item.SiteUrl -Force
+        if(Get-PnPSubWebs | where {$_.Url -eq $siteUrl}) 
+        {
+            Write-Host "Sub web $siteUrl exists. Moving to the next one." -ForegroundColor Yellow
+            continue
         }
         
-        # Create new site collection sub web.
-        New-PnPWeb -Template $item.Template -Title $item.Title -Url $item.SiteUrl -Locale 1033
+        # Creates new sub web.
+        New-PnPWeb -Template $item.Template -Title $item.Title -Url $item.SiteUrl -Locale $item.Locale
 
-        Write-Host "Sub web $siteUrl successfully created." -ForegroundColor Yellow
+        Write-Host "Sub web $siteUrl successfully created." -ForegroundColor Green
     }
 }
