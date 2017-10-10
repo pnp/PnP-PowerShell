25fc4d82c1c6f45a831b57d2595a23f2dcdd6574
Ivan Vagunin <ivan.vagunin@digia.com>
2017-10-10 13:47:57 +0300

    some changes

---
+++ Commands/Diagnostic/ListStatistics.cs
@@ -0,0 +1,23 @@
+﻿namespace SharePointPnP.PowerShell.Commands.Diagnostic
+{
+    public class ListStatistics
+    {
+        public int ItemCount { get; set; }
+        public int ItemVersionCount { get; set; }
+        public int FolderCount { get; set; }
+        public long TotalFileSize { get; set; }
+        public int BrokenPermissionCount { get; set; }
+
+        public static ListStatistics operator +(ListStatistics s1, FolderStatistics s2)
+        {
+            return new ListStatistics
+            {
+                ItemCount = s1.ItemCount,
+                ItemVersionCount = s1.ItemVersionCount + s2.ItemVersionCount,
+                FolderCount = s1.FolderCount + s2.FolderCount,
+                TotalFileSize = s1.TotalFileSize + s2.TotalFileSize,
+                BrokenPermissionCount = s1.BrokenPermissionCount + s2.BrokenPermissionCount
+            };
+        }
+    }
+}
--- Commands/Diagnostic/MeasurePnPWeb.cs
+++ Commands/Diagnostic/MeasurePnPWeb.cs
@@ -14,22 +14,26 @@ namespace SharePointPnP.PowerShell.Commands.Diagnostic
         Category = CmdletHelpCategory.Diagnostic)]
     public class MeasurePnPWeb : PnPCmdlet
     {
+        private Expression<Func<Web, object>>[] _retreivalExpressions;
+
         [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 0)]
         public WebPipeBind Identity;
 
         [Parameter(Mandatory = false)]
         public SwitchParameter Recursive;
 
-        private Expression<Func<Web, object>>[] _retreivalExpressions;
+        [Parameter(Mandatory = false)]
+        public SwitchParameter IncludeHiddenList;
 
         public MeasurePnPWeb()
         {
             _retreivalExpressions = new Expression<Func<Web, object>>[] {
+                w => w.Url,
                 w => w.Webs.Include(sw => sw.HasUniqueRoleAssignments),
-                w => w.Lists.Include(l => l.ItemCount, l => l.HasUniqueRoleAssignments),
-                w => w.Folders.Include(f => f.StorageMetrics, f => f.Files, f => f.ItemCount),
+                w => w.Lists.Include(l => l.Title, l => l.ItemCount, l => l.HasUniqueRoleAssignments, l => l.Hidden),
                 w => w.SiteUsers.Include(u => u.Id),
-                w => w.SiteGroups.Include(g => g.Id)
+                w => w.SiteGroups.Include(g => g.Id),
+                w => w.Folders
             };
         }
 
@@ -40,21 +44,65 @@ namespace SharePointPnP.PowerShell.Commands.Diagnostic
             WriteObject(statistics);
         }
 
+        private void WriteProgress(ProgressRecord record, string message, int step, int count)
+        {
+            var percentage = Convert.ToInt32((100 / Convert.ToDouble(count)) * Convert.ToDouble(step));
+            record.StatusDescription = message;
+            record.PercentComplete = percentage;
+            record.RecordType = ProgressRecordType.Processing;
+            WriteProgress(record);
+        }
+
+        private FolderStatistics GetFolderStatistics(Folder folder)
+        {
+            folder.EnsureProperties(
+                f => f.Name,
+                f => f.Files.Include(fl => fl.Length, fl => fl.Versions.Include(v=> v.Length)),
+                f => f.Folders.Include(fl => fl.Name));
+            var stat = new FolderStatistics
+            {
+                ItemCount = folder.Files.Count,
+                TotalFileSize = folder.Files.Sum(fl => fl.Length + ((fl.Versions != null) ? fl.Versions.Sum(v => v.Length) : 0))
+            };
+
+            foreach (var subfolder in folder.Folders)
+            {
+                stat += GetFolderStatistics(subfolder);
+            }
+            return stat;
+        }
+
         private WebStatistics GetStatistics(Web web)
         {
+            var progress = new ProgressRecord(0, $"Getting statistics for {web.Url}", "Retrieving web data");
+
+            WriteProgress(progress, $"Retrieving web data", 0, web.Folders.Count);
+
+            var lists = IncludeHiddenList ? web.Lists : web.Lists.Where(l => !l.Hidden);
+            var uniqueLists = lists.Where(l => l.HasUniqueRoleAssignments);
+
             var stat = new WebStatistics
             {
                 WebCount = web.Webs.Count,
-                ListCount = web.Lists.Count,
-                FolderCount = web.Folders.Count,
-                ItemCount = web.Lists.Sum(l => l.ItemCount),
-                ItemCount2 = web.Folders.Sum(f => f.ItemCount),
-                FileCount = web.Folders.Sum(f => f.Files.Count),
-                TotalSize = (int)web.Folders.Sum(f => f.StorageMetrics.TotalSize),
+                ListCount = lists.Count(),
+                ItemCount = lists.Sum(l => l.ItemCount),
                 SiteUserCount = web.SiteUsers.Count,
                 SiteGroupCount = web.SiteGroups.Count,
-                UniquePermissionCount = web.Lists.Count(l => l.HasUniqueRoleAssignments) + web.Webs.Count(w => w.HasUniqueRoleAssignments)
+                BrokenPermissionCount = uniqueLists.Count() + web.Webs.Count(w => w.HasUniqueRoleAssignments),
             };
+
+            var i = 0;
+            foreach(var folder in web.Folders)
+            {
+                stat += GetFolderStatistics(folder);
+                WriteProgress(progress, $"Retrieving folder data {folder.Name}", i++, web.Folders.Count);
+            }
+
+            foreach(var list in uniqueLists)
+            {
+                WriteVerbose($"List {list.Title} has unique permissions");
+            }
+
             if (Recursive)
             {
                 foreach (var subweb in web.Webs)
@@ -63,6 +111,10 @@ namespace SharePointPnP.PowerShell.Commands.Diagnostic
                     stat += GetStatistics(subweb);
                 }
             }
+
+            progress.RecordType = ProgressRecordType.Completed;
+            WriteProgress(progress);
+
             return stat;
         }
 
