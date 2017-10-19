f2140646afe3e11458348ed09114d95e267e8994
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-10-19 11:51:45 +0200

    Updated documentation

--- Commands/Principals/GetGroupMembers.cs
+++ Commands/Principals/GetGroupMembers.cs
@@ -5,28 +5,27 @@ using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
 
 namespace SharePointPnP.PowerShell.Commands.Principals
 {
-    [Cmdlet(VerbsCommon.Get, "PnPUsersInGroup")]
-    [CmdletHelp("Retrieves all users in a group",
+    [Cmdlet(VerbsCommon.Get, "PnPGroupMembers")]
+    [CmdletHelp("Retrieves all members of a group",
         Category = CmdletHelpCategory.Principals,
         DetailedDescription = "This command will return all the users that are a member of the provided SharePoint Group",
         OutputType = typeof(User),
         OutputTypeLink = "https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.user.aspx")]
     [CmdletExample(
-        Code = @"PS:> Get-PnPUserInGroup -Identity 'Marketing Site Members'",
+        Code = @"PS:> Get-PnPGroupMembers -Identity 'Marketing Site Members'",
         SortOrder = 1,
         Remarks = @"Returns all the users that are a member of the group 'Marketing Site Members' in the current sitecollection")]
     [CmdletExample(
-        Code = @"PS:> Get-PnPGroup | Get-PnPUsersInGroup",
+        Code = @"PS:> Get-PnPGroup | Get-PnPGroupMembers",
         SortOrder = 2,
         Remarks = @"Returns all the users that are a member of any of the groups in the current sitecollection")]
     [CmdletExample(
-        Code = @"PS:> Get-PnPGroup | ? Title -Like 'Marketing*' | Get-PnPUsersInGroup",
+        Code = @"PS:> Get-PnPGroup | ? Title -Like 'Marketing*' | Get-PnPGroupMembers",
         SortOrder = 3,
         Remarks = @"Returns all the users that are a member of any of the groups of which their name starts with the word 'Marketing' in the current sitecollection")]
-    public class GetUsersInGroup : PnPWebCmdlet
+    public class GetGroupMembers : PnPWebCmdlet
     {
         [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "A group object, an ID or a name of a group")]
-        [Alias("GroupName")]
         public GroupPipeBind Identity;
 
         protected override void ExecuteCmdlet()
---
+++ Documentation/Get-PnPGroupMembers.md
@@ -0,0 +1,76 @@
+---
+external help file:
+applicable: SharePoint Server 2013, SharePoint Server 2016, SharePoint Online
+schema: 2.0.0
+---
+# Get-PnPGroupMembers
+
+## SYNOPSIS
+Retrieves all members of a group
+
+## SYNTAX 
+
+```powershell
+Get-PnPGroupMembers -Identity <GroupPipeBind>
+                    [-Web <WebPipeBind>]
+```
+
+## DESCRIPTION
+This command will return all the users that are a member of the provided SharePoint Group
+
+## EXAMPLES
+
+### ------------------EXAMPLE 1------------------
+```powershell
+PS:> Get-PnPGroupMembers -Identity 'Marketing Site Members'
+```
+
+Returns all the users that are a member of the group 'Marketing Site Members' in the current sitecollection
+
+### ------------------EXAMPLE 2------------------
+```powershell
+PS:> Get-PnPGroup | Get-PnPGroupMembers
+```
+
+Returns all the users that are a member of any of the groups in the current sitecollection
+
+### ------------------EXAMPLE 3------------------
+```powershell
+PS:> Get-PnPGroup | ? Title -Like 'Marketing*' | Get-PnPGroupMembers
+```
+
+Returns all the users that are a member of any of the groups of which their name starts with the word 'Marketing' in the current sitecollection
+
+## PARAMETERS
+
+### -Identity
+A group object, an ID or a name of a group
+
+```yaml
+Type: GroupPipeBind
+Parameter Sets: (All)
+
+Required: True
+Position: Named
+Accept pipeline input: True
+```
+
+### -Web
+The GUID, server relative url (i.e. /sites/team1) or web instance of the web to apply the command to. Omit this parameter to use the current web.
+
+```yaml
+Type: WebPipeBind
+Parameter Sets: (All)
+
+Required: False
+Position: Named
+Accept pipeline input: False
+```
+
+## OUTPUTS
+
+### [Microsoft.SharePoint.Client.User](https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.user.aspx)
+
+# RELATED LINKS
+
+[SharePoint Developer Patterns and Practices](http://aka.ms/sppnp)
