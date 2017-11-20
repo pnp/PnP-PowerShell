67ec5ef8f27bc1567bf8b6a36c15f4a8327ccde8
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-11-20 17:59:21 +0100

    Added storage entity / farm property cmdlets

---
+++ Commands/Admin/RemoveStorageEntity.cs
@@ -0,0 +1,36 @@
+﻿#if !ONPREMISES
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Online.SharePoint.TenantAdministration;
+using Microsoft.SharePoint.Client;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base;
+using SharePointPnP.PowerShell.Commands.Enums;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SharePointPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Remove, "PnPStorageEntity", SupportsShouldProcess = true)]
+    [CmdletHelp(@"Remove Storage Entities / Farm Properties.",
+        Category = CmdletHelpCategory.TenantAdmin,
+        SupportedPlatform = CmdletSupportedPlatform.Online)]
+    [CmdletExample(Code = @"PS:> Remove-PnPStorageEntity -Key MyKey ", Remarks = "Removes an existing storage entity / farm property", SortOrder = 1)]   
+    public class RemovePnPStorageEntity : PnPCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "The key of the value to set.")]
+        public string Key;
+
+        protected override void ExecuteCmdlet()
+        {          
+            var appCatalogUri = ClientContext.Web.GetAppCatalog();
+            using (var clonedContext = ClientContext.Clone(appCatalogUri))
+            {
+                clonedContext.Web.RemoveStorageEntity(Key);
+                clonedContext.ExecuteQueryRetry();
+            }
+        }
+    }
+}
+#endif
--- Documentation/readme.md
+++ Documentation/readme.md
@@ -275,6 +275,9 @@ Cmdlet|Description|Platforms
 :-----|:----------|:--------
 **[Get&#8209;PnPAccessToken](Get-PnPAccessToken.md)** |Returns the current OAuth Access token|All
 **[New&#8209;PnPSite](New-PnPSite.md)** |BETA: This cmdlet is using early release APIs. Notice that functionality and parameters can change. Creates a new site collection|SharePoint Online
+**[Get&#8209;PnPStorageEntity](Get-PnPStorageEntity.md)** |Retrieve Storage Entities / Farm Properties.|SharePoint Online
+**[Remove&#8209;PnPStorageEntity](Remove-PnPStorageEntity.md)** |Remove Storage Entities / Farm Properties.|SharePoint Online
+**[Set&#8209;PnPStorageEntity](Set-PnPStorageEntity.md)** |Set Storage Entities / Farm Properties.|SharePoint Online
 **[Clear&#8209;PnPTenantRecycleBinItem](Clear-PnPTenantRecycleBinItem.md)** |Permanently deletes a site collection from the tenant scoped recycle bin|All
 **[Restore&#8209;PnPTenantRecycleBinItem](Restore-PnPTenantRecycleBinItem.md)** |Restores a site collection from the tenant scoped recycle bin|SharePoint Online
 **[Get&#8209;PnPTenantSite](Get-PnPTenantSite.md)** |Retrieve site information.|SharePoint Online
