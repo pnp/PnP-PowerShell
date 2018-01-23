8aab9ec9adeaffad19696b1b91dd5d028e8b8b69
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-01-24 00:32:32 +0100

    Removed unnessary parameter

--- Commands/Admin/GetTenantAppCatalogUrl.cs
+++ Commands/Admin/GetTenantAppCatalogUrl.cs
@@ -19,9 +19,6 @@ namespace SharePointPnP.PowerShell.Commands
     [CmdletExample(Code = @"PS:> Get-PnPTenantAppCatalogUrl", Remarks = "Returns the url of the tenant scoped app catalog site collection", SortOrder = 1)]
     public class GetTenantAppCatalogUrl : PnPCmdlet
     {
-        [Parameter(Mandatory = false, HelpMessage = "The key of the value to retrieve.")]
-        public string Key;
-
         protected override void ExecuteCmdlet()
         {
             var settings = Microsoft.SharePoint.Client.TenantSettings.GetCurrent(ClientContext);
