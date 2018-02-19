798a76d4c542bb3a9f690488f34d82ab7e9e23d9
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-02-19 23:10:36 +0100

    Added DeviceLogin option

---
+++ Commands/Utilities/Shell.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SharePointPnP.PowerShell.Commands.Utilities
+{
+    public static class Shell
+    {
+        public static string Bash(string cmd)
+        {
+            string result = Run("/bin/bash", $@"-c ""{cmd}""");
+            return result;
+        }
+
+        public static string Bat(string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            string result = Run("cmd.exe", $@"/c ""{escapedArgs}""");
+            return result;
+        }
+
+
+        private static string Run(string filename, string arguments)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
