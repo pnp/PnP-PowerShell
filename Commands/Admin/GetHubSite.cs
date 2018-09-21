d0090f0882390c00a8f83a8ab476af7788609332
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-09-21 11:23:10 +0200

    updated examples

--- Commands/Admin/GetHubSite.cs
+++ Commands/Admin/GetHubSite.cs
@@ -13,8 +13,8 @@ namespace SharePointPnP.PowerShell.Commands.Admin
     [CmdletHelp(@"Retrieve all or a specific hubsite.",
         Category = CmdletHelpCategory.TenantAdmin,
         SupportedPlatform = CmdletSupportedPlatform.Online)]
-    [CmdletExample(Code = @"PS:> Get-PnPStorageEntity", Remarks = "Returns all site storage entities/farm properties", SortOrder = 1)]
-    [CmdletExample(Code = @"PS:> Get-PnPTenantSite -Key MyKey", Remarks = "Returns the storage entity/farm property with the given key.", SortOrder = 2)]
+    [CmdletExample(Code = @"PS:> Get-PnPHubSite", Remarks = "Returns all hubsite properties", SortOrder = 1)]
+    [CmdletExample(Code = @"PS:> Get-PnPHubSite -Identity https://contoso.sharepoint.com/sites/myhubsite", Remarks = "Returns the properties of the specified hubsite", SortOrder = 2)]
     public class GetHubSite : PnPAdminCmdlet
     {
         [Parameter(Position = 0, ValueFromPipeline = true)]
