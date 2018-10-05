5f26660f7c917c6b56a527cebcc9c65b57dd7d44
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-10-05 10:22:15 +0200

    updated documentation

--- Commands/Provisioning/AddProvisioningSubSite.cs
+++ Commands/Provisioning/AddProvisioningSubSite.cs
@@ -11,19 +11,15 @@ namespace SharePointPnP.PowerShell.Commands.Provisioning
     [CmdletHelp("Adds a provisioning sequence object to a provisioning site object",
         Category = CmdletHelpCategory.Provisioning, SupportedPlatform = CmdletSupportedPlatform.Online)]
     [CmdletExample(
-       Code = @"PS:> Add-PnPProvisioningSequence -Hierarchy $myhierarchy -Sequence $mysequence",
-       Remarks = "Adds an existing sequence object to an existing hierarchy object",
+       Code = @"PS:> Add-PnPProvisioningSubSite -Site $mysite -SubSite $mysubsite",
+       Remarks = "Adds an existing subsite object to an existing hierarchy sequence site object",
        SortOrder = 1)]
-    [CmdletExample(
-       Code = @"PS:> New-PnPProvisioningSequence -Id ""MySequence"" | Add-PnPProvisioningSequence -Hierarchy $hierarchy",
-       Remarks = "Creates a new instance of a provisioning sequence object and sets the Id to the value specified, then the sequence is added to an existing hierarchy object",
-       SortOrder = 2)]
     public class AddProvisioningSubSite : PSCmdlet
     {
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, HelpMessage = "The subsite to add")]
         public TeamNoGroupSubSite SubSite;
 
-        [Parameter(Mandatory = true, HelpMessage = "The sequence to add the site to", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, HelpMessage = "The site to add the subsite to", ValueFromPipeline = true)]
         public SiteCollection Site;
 
         protected override void ProcessRecord()
