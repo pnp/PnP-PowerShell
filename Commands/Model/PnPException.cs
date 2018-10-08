324b92121a9118bed777889ca966f4e4838e9712
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-10-08 11:23:34 +0200

    Added Get-PnPException cmdlet

---
+++ Commands/Model/PnPException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.PowerShell.Commands.Model
+{
+    public class PnPException
+    {
+        public string Message;
+        public string Stacktrace;
+        public int ScriptLineNumber;
+        public InvocationInfo InvocationInfo;
+    }
+}
