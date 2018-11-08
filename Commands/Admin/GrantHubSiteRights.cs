0a6d1a67bd1ad685f4af322849861f4600d55a0c
Koen Zomers <koen@zomers.eu>
2018-11-08 10:07:49 +0100

    Extra clarification that permissions are added to existing permissions and do not overwrite what has been set before

--- Commands/Admin/GrantHubSiteRights.cs
+++ Commands/Admin/GrantHubSiteRights.cs
@@ -9,7 +9,7 @@ using System.Management.Automation;
 namespace SharePointPnP.PowerShell.Commands.Admin
 {
     [Cmdlet(VerbsSecurity.Grant, "PnPHubSiteRights")]
-    [CmdletHelp(@"Grant Permissions to associate sites to Hub Sites for one or more specific users",
+    [CmdletHelp(@"Grant additional permissions to the permissions already in place to associate sites to Hub Sites for one or more specific users",
         Category = CmdletHelpCategory.TenantAdmin,
         SupportedPlatform = CmdletSupportedPlatform.Online)]
     [CmdletExample(Code = @"PS:> Grant-PnPHubSiteRights -Identity https://contoso.sharepoint.com/sites/hubsite -Principals ""myuser@mydomain.com"",""myotheruser@mydomain.com"" -Rights Join", Remarks = "This example shows how to grant rights to myuser and myotheruser to associate their sites with the provided Hub Site", SortOrder = 1)]
@@ -20,7 +20,7 @@ namespace SharePointPnP.PowerShell.Commands.Admin
         [Alias("HubSite")]
         public HubSitePipeBind Identity { get; set; }
 
-        [Parameter(Mandatory = true, HelpMessage = "One or more usernames that will be given or revoked the permission to associate a site with this Hub Site")]
+        [Parameter(Mandatory = true, HelpMessage = "One or more usernames that will be given or revoked the permission to associate a site with this Hub Site. It does not replace permissions given out before but adds to the already existing permissions.")]
         public string[] Principals { get; set; }
 
         [Parameter(Mandatory = true, HelpMessage = "Provide Join to give permissions to associate a site with this Hub Site or use None to revoke the permissions for the user(s) specified with the Principals argument")]
