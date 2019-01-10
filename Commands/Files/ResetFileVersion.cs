25cdf8b4a3a5e08aec0596aafb17c4a99c90caf3
Erwin van Hunen <erwin.van.hunen@outlook.com>
2019-01-10 14:26:51 +0100

    Minor updates to documentation

--- Commands/Files/ResetFileVersion.cs
+++ Commands/Files/ResetFileVersion.cs
@@ -5,12 +5,12 @@ using System.Management.Automation;
 namespace SharePointPnP.PowerShell.Commands.Files
 {
     [Cmdlet(VerbsCommon.Reset, "PnPFileVersion")]
-    [CmdletHelp("Resets a file to its previous version", Category = CmdletHelpCategory.Files)]
+    [CmdletHelp("Resets a file to its previous version", Category = CmdletHelpCategory.Files, SupportedPlatform = CmdletSupportedPlatform.All)]
     [CmdletExample(
-        Code = @"PS:> Reset-PnPFileVersion -ServerRelativeUrl ""/office365.png""",
+        Code = @"PS:> Reset-PnPFileVersion -ServerRelativeUrl ""/sites/test/office365.png""",
         SortOrder = 1)]
     [CmdletExample(
-        Code = @"PS:> Reset-PnPFileVersion -ServerRelativeUrl ""/office365.png"" -CheckinType MajorCheckin -Comment ""Restored to previous version""",
+        Code = @"PS:> Reset-PnPFileVersion -ServerRelativeUrl ""/sites/test/office365.png"" -CheckinType MajorCheckin -Comment ""Restored to previous version""",
         SortOrder = 2)]
     public class ResetFileVersion : PnPWebCmdlet
     {
@@ -20,7 +20,7 @@ namespace SharePointPnP.PowerShell.Commands.Files
         [Parameter(Mandatory = false, HelpMessage = @"The check in type to use. Defaults to Major.")]
         public CheckinType CheckinType = CheckinType.MajorCheckIn;
 
-        [Parameter(Mandatory = false, HelpMessage = "The comment added to the checkin.")]
+        [Parameter(Mandatory = false, HelpMessage = "The comment added to the checkin. Defaults to 'Restored to previous version'.")]
         public string CheckInComment = "Restored to previous version";
 
         protected override void ExecuteCmdlet()
