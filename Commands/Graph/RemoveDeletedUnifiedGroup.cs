0096c3d58b7bfc5edf622ced31909553ee9538fa
Laura Kokkarinen <laura.kokkarinen@outlook.com>
2019-07-31 17:53:53 +0300

    Added commandlets for managing (get, restore, remove) deleted Office 365 groups + unit tests.

---
+++ Commands/Graph/RemoveDeletedUnifiedGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Management.Automation;
+using OfficeDevPnP.Core.Framework.Graph;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base;
+using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace SharePointPnP.PowerShell.Commands.Graph
+{
+    [Cmdlet(VerbsCommon.Remove, "PnPDeletedUnifiedGroup")]
+
+    [CmdletHelp("Permanently removes one deleted Office 365 Group (aka Unified Group)",
+        Category = CmdletHelpCategory.Graph,
+        SupportedPlatform = CmdletSupportedPlatform.Online)]
+    [CmdletExample(
+        Code = "PS:> Remove-PnPDeletedUnifiedGroup -Identity $groupId",
+        Remarks = "Permanently removes a deleted Office 365 Group based on its ID",
+        SortOrder = 1)]
+    [CmdletExample(
+        Code = "PS:> Remove-PnPDeletedUnifiedGroup -Identity $group",
+        Remarks = "Permanently removes the provided deleted Office 365 Group",
+        SortOrder = 2)]
+
+    public class RemoveDeletedUnifiedGroup : PnPGraphCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Identity of the deleted Office 365 Group")]
+        public UnifiedGroupPipeBind Identity;
+
+        protected override void ExecuteCmdlet()
+        {
+            var group = Identity?.GetDeletedGroup(AccessToken);
+
+            if (group != null)
+            {
+                UnifiedGroupsUtility.PermanentlyDeleteUnifiedGroup(group.GroupId, AccessToken);
+            }
+        }
+    }
+}
---
+++ Commands/Graph/RestoreDeletedUnifiedGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Management.Automation;
+using OfficeDevPnP.Core.Framework.Graph;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base;
+using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace SharePointPnP.PowerShell.Commands.Graph
+{
+    [Cmdlet(VerbsData.Restore, "PnPDeletedUnifiedGroup")]
+
+    [CmdletHelp("Restores one deleted Office 365 Group (aka Unified Group)",
+        Category = CmdletHelpCategory.Graph,
+        SupportedPlatform = CmdletSupportedPlatform.Online)]
+    [CmdletExample(
+        Code = "PS:> Restore-PnPDeletedUnifiedGroup -Identity $groupId",
+        Remarks = "Restores a deleted Office 365 Group based on its ID",
+        SortOrder = 1)]
+    [CmdletExample(
+        Code = "PS:> Restore-PnPDeletedUnifiedGroup -Identity $group",
+        Remarks = "Restores the provided deleted Office 365 Group",
+        SortOrder = 2)]
+
+    public class RestoreDeletedUnifiedGroup : PnPGraphCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Identity of the deleted Office 365 Group")]
+        public UnifiedGroupPipeBind Identity;
+
+        protected override void ExecuteCmdlet()
+        {
+            var group = Identity?.GetDeletedGroup(AccessToken);
+
+            if (group != null)
+            {
+                UnifiedGroupsUtility.RestoreDeletedUnifiedGroup(group.GroupId, AccessToken);
+            }
+        }
+    }
+}
