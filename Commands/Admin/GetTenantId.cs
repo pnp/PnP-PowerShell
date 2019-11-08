bb912f3e285256d8fa0bbc767648702e6ff00371
Erwin van Hunen <erwin.van.hunen@outlook.com>
2019-11-08 12:49:50 +0100

    Refactored Get-PnPTenantId to work without a clientcontext

--- Commands/Admin/GetTenantId.cs
+++ Commands/Admin/GetTenantId.cs
@@ -1,28 +1,70 @@
 ﻿#if !ONPREMISES
 using Microsoft.SharePoint.Client;
+using Newtonsoft.Json.Linq;
 using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SharePointPnP.PowerShell.Commands.Admin
 {
     [Cmdlet(VerbsCommon.Get, "PnPTenantId")]
     [CmdletHelp(@"Returns the Tenant ID",
-        DetailedDescription = @"Returns the current Tenant Id for the tenant currently connected to.",
         SupportedPlatform = CmdletSupportedPlatform.Online,
         Category = CmdletHelpCategory.TenantAdmin)]
     [CmdletExample(
         Code = @"PS:> Get-PnPTenantId",
-        Remarks = @"Returns the current Tenant Id", SortOrder = 1)]
-    public class GetTenantId : PnPCmdlet
+        Remarks = @"Returns the current Tenant Id. A valid connection with Connect-PnPOnline is required.", SortOrder = 1)]
+    [CmdletExample(
+        Code = @"PS:> Get-PnPTenantId -TenantUrl https://contoso.sharepoint.com",
+        Remarks = @"Returns the Tenant ID for the specified tenant. Can be executed without a connecting first with Connect-PnPOnline", SortOrder = 1)]
+    public class GetTenantId : BasePSCmdlet
     {
-        protected override void ExecuteCmdlet()
+        [Parameter(Mandatory = false)]
+        public string TenantUrl;
+
+        protected override void ProcessRecord()
         {
-            WriteObject(TenantExtensions.GetTenantIdByUrl(ClientContext.Url));
+            try
+            {
+                if (string.IsNullOrEmpty(TenantUrl) && SPOnlineConnection.CurrentConnection != null)
+                {
+                    WriteObject(TenantExtensions.GetTenantIdByUrl(SPOnlineConnection.CurrentConnection.Url));
+                }
+                else if (!string.IsNullOrEmpty(TenantUrl))
+                {
+                    WriteObject(TenantExtensions.GetTenantIdByUrl(TenantUrl));
+                }
+                else
+                {
+                    throw new InvalidOperationException("Either a connection needs to be made by Connect-PnPOnline or TenantUrl needs to be specified");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    if (ex.InnerException is HttpException)
+                    {
+                        var message = ex.InnerException.Message;
+                        var obj = JObject.Parse(message);
+                        WriteObject(obj["error_description"].ToString());
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }
---
+++ Commands/Base/BasePSCmdlet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.PowerShell.Commands.Base
+{
+    public class BasePSCmdlet : PSCmdlet
+    {
+        private Assembly newtonsoftAssembly;
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            FixAssemblyResolving();
+        }
+
+        private void FixAssemblyResolving()
+        {
+            newtonsoftAssembly = System.Reflection.Assembly.LoadFrom(Path.Combine(AssemblyDirectory, "NewtonSoft.Json.dll"));
+            System.AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+        }
+
+        private string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        private System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+
+            if (args.Name.StartsWith("Newtonsoft.Json"))
+            {
+                return newtonsoftAssembly;
+            }
+            foreach (var assembly in System.AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assembly.FullName == args.Name)
+                {
+                    return assembly;
+                }
+            }
+            return null;
+        }
+    }
+}
