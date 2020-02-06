85acedc7f13ae0b57fb4dc902b2522984cde7329
Koen Zomers <koen@zomers.eu>
2020-02-07 00:56:55 +0100

    Code sanitation

--- Commands/Admin/GetWebTemplates.cs
+++ Commands/Admin/GetWebTemplates.cs
@@ -6,11 +6,11 @@ using System.Management.Automation;
 namespace SharePointPnP.PowerShell.Commands
 {
     [Cmdlet(VerbsCommon.Get, "PnPWebTemplates")]
-    [CmdletHelp(@"Returns the available web templates.", 
-        "Will list all available templates one can use to create a classic site.",
+    [CmdletHelp(@"Returns the available web templates", 
+        "Will list all available templates one can use to create a site",
         Category = CmdletHelpCategory.TenantAdmin,
         SupportedPlatform = CmdletSupportedPlatform.All,
-        OutputType =typeof(Microsoft.Online.SharePoint.TenantAdministration.SPOTenantWebTemplateCollection),
+        OutputType = typeof(Microsoft.Online.SharePoint.TenantAdministration.SPOTenantWebTemplateCollection),
         OutputTypeLink = "https://docs.microsoft.com/en-us/previous-versions/office/sharepoint-csom/dn174817(v=office.15)")]
     [CmdletExample(Code = @"PS:> Get-PnPWebTemplates", SortOrder = 1)]
     [CmdletExample(Code = @"PS:> Get-PnPWebTemplates -LCID 1033", Remarks = @"Returns all webtemplates for the Locale with ID 1033 (English)", SortOrder = 2)]
@@ -18,7 +18,7 @@ namespace SharePointPnP.PowerShell.Commands
     [CmdletRelatedLink(Text = "Locale IDs", Url = "http://go.microsoft.com/fwlink/p/?LinkId=242911Id=242911")]
     public class GetWebTemplates : PnPAdminCmdlet
     {
-        [Parameter(Mandatory = false, HelpMessage = "The language ID. For instance: 1033 for English")]
+        [Parameter(Mandatory = false, HelpMessage = "The language ID. For instance: 1033 for English.")]
         public uint Lcid;
 
         [Parameter(Mandatory = false, HelpMessage = "The version of SharePoint")]
