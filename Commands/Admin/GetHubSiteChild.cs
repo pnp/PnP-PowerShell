7d640a89b5ac93af2071f5fea1137acaf935408a
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2020-05-05 13:12:54 -0400

    Update GetHubSiteChild.cs
    Example 1 had misspelled cmdlet name: Get-PnPHubChildSite vs. Get-PnPHubSiteChild

--- Commands/Admin/GetHubSiteChild.cs
+++ Commands/Admin/GetHubSiteChild.cs
@@ -15,7 +15,7 @@ namespace SharePointPnP.PowerShell.Commands.Admin
         "Retrieves all sites linked to a specific hub site",
         Category = CmdletHelpCategory.TenantAdmin,
         SupportedPlatform = CmdletSupportedPlatform.Online)]
-    [CmdletExample(Code = @"PS:> Get-PnPHubChildSite -Identity https://contoso.sharepoint.com/sites/myhubsite", Remarks = "Returns the sites having configured the provided hub site as their hub site", SortOrder = 1)]
+    [CmdletExample(Code = @"PS:> Get-PnPHubSiteChild -Identity https://contoso.sharepoint.com/sites/myhubsite", Remarks = "Returns the sites having configured the provided hub site as their hub site", SortOrder = 1)]
     public class GetHubSiteChild : PnPAdminCmdlet
     {
         [Parameter(ValueFromPipeline = true, Mandatory = true, HelpMessage = "The URL of the hubsite for which to receive the sites refering to it")]
