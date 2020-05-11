7b8afb87f2ac15540755842cae363acc2025b68e
Koen Zomers <koen@zomers.eu>
2020-05-11 17:16:05 +0200

    Renamed Office 365 Group to Microsoft 365 Group following the name change applied by Microsoft

--- Commands/Graph/GetUnifiedGroupMembers.cs
+++ Commands/Graph/GetUnifiedGroupMembers.cs
@@ -10,20 +10,20 @@ using System.Management.Automation;
 namespace SharePointPnP.PowerShell.Commands.Graph
 {
     [Cmdlet(VerbsCommon.Get, "PnPUnifiedGroupMembers")]
-    [CmdletHelp("Gets members of a particular Office 365 Group (aka Unified Group). Requires the Azure Active Directory application permissions 'Group.Read.All' and 'User.Read.All'.",
+    [CmdletHelp("Gets members of a particular Microsoft 365 Group (aka Unified Group). Requires the Azure Active Directory application permissions 'Group.Read.All' and 'User.Read.All'.",
         Category = CmdletHelpCategory.Graph,
         SupportedPlatform = CmdletSupportedPlatform.Online)]
     [CmdletExample(
        Code = "PS:> Get-PnPUnifiedGroupMembers -Identity $groupId",
-       Remarks = "Retrieves all the members of a specific Office 365 Group based on its ID",
+       Remarks = "Retrieves all the members of a specific Microsoft 365 Group based on its ID",
        SortOrder = 1)]
     [CmdletExample(
        Code = "PS:> Get-PnPUnifiedGroupMembers -Identity $group",
-       Remarks = "Retrieves all the members of a specific Office 365 Group based on the group's object instance",
+       Remarks = "Retrieves all the members of a specific Microsoft 365 Group based on the group's object instance",
        SortOrder = 2)]
     public class GetUnifiedGroupMembers : PnPGraphCmdlet
     {
-        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Identity of the Office 365 Group.")]
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Identity of the Microsoft 365 Group")]
         public UnifiedGroupPipeBind Identity;
 
         protected override void ExecuteCmdlet()
