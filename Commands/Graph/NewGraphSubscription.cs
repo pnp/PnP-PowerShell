99018a623b023946a88df5bbf1d4c701b03917d3
Koen Zomers <koen@zomers.eu>
2020-05-12 11:41:03 +0200

    Updated help text to provide more samples

--- Commands/Graph/NewGraphSubscription.cs
+++ Commands/Graph/NewGraphSubscription.cs
@@ -8,7 +8,8 @@ using System.Management.Automation;
 namespace SharePointPnP.PowerShell.Commands.Graph
 {
     [Cmdlet(VerbsCommon.New, "PnPGraphSubscription")]
-    [CmdletHelp("Creates a new Microsof Graph Subscription. The required Azure Active Directory application permission depend on the resource creating the subscription for, see https://docs.microsoft.com/graph/api/subscription-post-subscriptions#permissions.",
+    [CmdletHelp("Creates a new Microsof Graph Subscription which allows your webhook API to be called when a change occurs in Microsoft Graph",
+        DetailedDescription = "Creates a new Microsof Graph Subscription. The required Azure Active Directory application permission depends on the resource creating the subscription for, see https://docs.microsoft.com/graph/api/subscription-post-subscriptions#permissions. For a sample ASP.NET WebApi webhook implementation to receive the notifications from Microsoft Graph, see https://github.com/microsoftgraph/msgraph-training-changenotifications/blob/b8d21ca7aa5feeece336287c9a781e71b7ba01c6/demos/01-create-application/Controllers/NotificationsController.cs#L51.",
         Category = CmdletHelpCategory.Graph,
         OutputTypeLink = "https://docs.microsoft.com/graph/api/subscription-post-subscriptions",
         SupportedPlatform = CmdletSupportedPlatform.Online)]
@@ -16,6 +17,10 @@ namespace SharePointPnP.PowerShell.Commands.Graph
        Code = "PS:> New-PnPGraphSubscription -ChangeType Create -NotificationUrl https://mywebapiservice/notifications -Resource \"me/mailFolders('Inbox')/messages\" -ExpirationDateTime (Get-Date).AddDays(1) -ClientState [Guid]::NewGuid().ToString()",
        Remarks = "Creates a new Microsoft Graph subscription listening for incoming mail during the next 24 hours in the inbox of the user under which the connection has been made and will signal the URL provided through NotificationUrl when a message comes in",
        SortOrder = 1)]
+    [CmdletExample(
+       Code = "PS:> New-PnPGraphSubscription -ChangeType Updates -NotificationUrl https://mywebapiservice/notifications -Resource \"Users\" -ExpirationDateTime (Get-Date).AddHours(1) -ClientState [Guid]::NewGuid().ToString()",
+       Remarks = "Creates a new Microsoft Graph subscription listening for changes to user objects during the next hour and will signal the URL provided through NotificationUrl when a change has been made",
+       SortOrder = 2)]
     public class NewGraphSubscription : PnPGraphCmdlet
     {
         [Parameter(Mandatory = true, HelpMessage = "The event(s) the subscription should trigger on")]
@@ -24,7 +29,7 @@ namespace SharePointPnP.PowerShell.Commands.Graph
         [Parameter(Mandatory = true, HelpMessage = "The URL that should be called when an event matching this subscription occurs")]
         public String NotificationUrl;
 
-        [Parameter(Mandatory = true, HelpMessage = "The resource to monitor for changes. See https://docs.microsoft.com/graph/api/subscription-post-subscriptions#permissions for the list with supported options.")]
+        [Parameter(Mandatory = true, HelpMessage = "The resource to monitor for changes. See https://docs.microsoft.com/graph/api/subscription-post-subscriptions#resources-examples for the list with supported options.")]
         public String Resource;
 
         [Parameter(Mandatory = false, HelpMessage = "The datetime defining how long this subscription should stay alive before which it needs to get extended to stay alive. See https://docs.microsoft.com/graph/api/resources/subscription#maximum-length-of-subscription-per-resource-type for the supported maximum lifetime of the subscriber endpoints.")]
