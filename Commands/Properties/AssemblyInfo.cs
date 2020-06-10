ba472f1a40297fe7832ce5fc1df4abbe29a531b4
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-06-10 12:52:20 +0200

    fixes issue with -PnPO365ManagementShell parameter and access denied messages

--- Commands/Properties/AssemblyInfo.cs
+++ Commands/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.22.2006.1")]
-[assembly: AssemblyFileVersion("3.22.2006.1")]
+[assembly: AssemblyVersion("3.22.2006.2")]
+[assembly: AssemblyFileVersion("3.22.2006.2")]
 [assembly: InternalsVisibleTo("SharePointPnP.PowerShell.Tests")]
