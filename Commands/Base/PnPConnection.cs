713d3dc36ac6dd01fb62b98e6b9a053bbf6ec01e
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-07-17 11:01:49 +0200

    July 2020 Intermediate Release 1

--- Commands/Base/PnPConnection.cs
+++ Commands/Base/PnPConnection.cs
@@ -237,7 +237,7 @@ namespace SharePointPnP.PowerShell.Commands.Base
                                 var scopes = Scopes.Except(officeManagementApiScopes).ToArray();
                                 if (scopes.Length > 0)
                                 {
-                                    token = PSCredential == null ? GraphToken.AcquireApplicationTokenInteractive(DeviceLoginClientId, Scopes) : GraphToken.AcquireDelegatedTokenWithCredentials(DeviceLoginClientId, scopes, PSCredential.UserName, PSCredential.Password);
+                                    token = PSCredential == null ? GraphToken.AcquireApplicationTokenInteractive(DeviceLoginClientId, scopes) : GraphToken.AcquireDelegatedTokenWithCredentials(DeviceLoginClientId, scopes, PSCredential.UserName, PSCredential.Password);
                                 }
                                 else
                                 {
@@ -265,7 +265,7 @@ namespace SharePointPnP.PowerShell.Commands.Base
                             // Take the remaining scopes and try requesting them from the Microsoft Graph API
                             if (scopes.Length > 0)
                             {
-                                token = PSCredential == null ? OfficeManagementApiToken.AcquireApplicationTokenInteractive(DeviceLoginClientId, Scopes) : OfficeManagementApiToken.AcquireDelegatedTokenWithCredentials(DeviceLoginClientId, scopes, PSCredential.UserName, PSCredential.Password);
+                                token = PSCredential == null ? OfficeManagementApiToken.AcquireApplicationTokenInteractive(DeviceLoginClientId, scopes) : OfficeManagementApiToken.AcquireDelegatedTokenWithCredentials(DeviceLoginClientId, scopes, PSCredential.UserName, PSCredential.Password);
                             }
                             else
                             {
