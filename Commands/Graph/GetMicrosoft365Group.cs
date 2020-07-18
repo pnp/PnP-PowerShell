c4ffa23150feebc143f88d5168a1dae1516a5408
Koen Zomers <koen@zomers.eu>
2020-07-18 09:47:44 +0200

    Updated IncludeHasTeam in example

--- Commands/Graph/GetMicrosoft365Group.cs
+++ Commands/Graph/GetMicrosoft365Group.cs
@@ -36,7 +36,7 @@ namespace SharePointPnP.PowerShell.Commands.Graph
        Remarks = "Retrieves a specific Microsoft 365 Group based on its object instance",
        SortOrder = 5)]
     [CmdletExample(
-       Code = "PS:> Get-PnPMicrosoft365Group -IncludeIfHasTeam",
+       Code = "PS:> Get-PnPMicrosoft365Group -IncludeHasTeam",
        Remarks = "Retrieves all the Microsoft 365 Groups and checks for each of them if it has a Microsoft Team provisioned for it",
        SortOrder = 6)]
     [CmdletMicrosoftGraphApiPermission(MicrosoftGraphApiPermission.Group_Read_All | MicrosoftGraphApiPermission.Group_ReadWrite_All | MicrosoftGraphApiPermission.GroupMember_ReadWrite_All | MicrosoftGraphApiPermission.GroupMember_Read_All | MicrosoftGraphApiPermission.Directory_ReadWrite_All | MicrosoftGraphApiPermission.Directory_Read_All)]
