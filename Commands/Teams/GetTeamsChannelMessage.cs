0b78bf44c2dc039a02052c8b89990283b398f1f9
Aimery THOMAS <7100077+a1mery@users.noreply.github.com>
2020-09-16 16:22:01 +0200

    Add example getting channel messages

--- Commands/Teams/GetTeamsChannelMessage.cs
+++ Commands/Teams/GetTeamsChannelMessage.cs
@@ -13,8 +13,8 @@ namespace PnP.PowerShell.Commands.Graph
         Category = CmdletHelpCategory.Teams,
         SupportedPlatform = CmdletSupportedPlatform.Online)]
     [CmdletExample(
-       Code = "PS:> Submit-PnPTeamsChannelMessage -Team MyTestTeam -Channel \"My Channel\" -Message \"A new message\"",
-       Remarks = "Sends \"A new message\" to the specified channel",
+       Code = "PS:> Get-PnPTeamsChannelMessage -Team 07d9dbfb-cbf2-4dc1-92c6-7921f5931b3d -Channel \"My Channel\"",
+       Remarks = "Gets all messages of the specified channel",
        SortOrder = 1)]
     [CmdletMicrosoftGraphApiPermission(MicrosoftGraphApiPermission.Group_ReadWrite_All)]
     public class GetTeamsChannelMessage : PnPGraphCmdlet
